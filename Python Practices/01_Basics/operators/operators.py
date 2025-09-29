a09f9d921fd0221a8a3c0bea7bc4d965849f6aa4
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-09-29 21:41:13 +0600

    Uploaded all files

---
+++ Python Practices/01_Basics/operators/operators.py
@@ -0,0 +1,65 @@
+# operator and operand
+# operator = + # operator is an operator 
+# operand = 10 # operand is a value
+
+# operator precedence and associativity
+# operator precedence is the order in which operators are evaluated
+# associativity is the order in which operators are evaluated
+
+
+# 1. ======================= Arithmetic operators =======================
+# + addition
+# - subtraction
+# * multiplication
+# / division
+# % modulo
+# ** exponentiation
+
+
+# 2. ======================= Assignment operators =======================
+# = assignment
+# += addition assignment
+# -= subtraction assignment
+# *= multiplication assignment
+# /= division assignment
+# %= modulo assignment
+# **= exponentiation assignment
+
+# 3. ======================= Comparison operators =======================
+# == equal to
+# != not equal to
+# > greater than
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+
+
+# 4. ======================= Logical operators =======================
+# and logical and
+# or logical or
+# not logical not
+
+
+# 5. ======================= Bitwise operators =======================
+# & bitwise AND
+# | bitwise OR
+# ^ bitwise XOR
+# ~ bitwise NOT
+# << left shift
+# >> right shift
+
+
+# 6. ======================= Membership operators =======================
+# in membership
+# not in membership
+
+
+# 7. ======================= Identity operators =======================
+# is identity
+# is not identity
+
+
+# 8. ======================= Other operators =======================
+# yield yield
+# lambda lambda
+# @ operator
