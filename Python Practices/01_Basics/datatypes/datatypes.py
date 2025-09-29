9b8722dfc15bcece4f79ee5392eecb4976265981
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-09-29 20:19:33 +0600

    Uploaded all files

---
+++ Python Practices/01_Basics/datatypes/datatypes.py
@@ -0,0 +1,52 @@
+"""
+Python Datatypes (Quick Notes)
+==============================
+
+1. Numbers
+   - int      → 10, -5, 1000
+   - float    → 3.14, -0.5
+   - complex  → 2+3j
+
+2. Text
+   - str      → "Hello", 'Python'
+
+3. Boolean
+   - bool     → True, False
+
+4. Sequence Types
+   - list     → [1, 2, 3]
+   - tuple    → (1, 2, 3)
+   - range    → range(5) → 0 to 4
+
+5. Set Types
+   - set      → {1, 2, 3}
+   - frozenset→ frozenset({1, 2, 3})
+
+6. Mapping
+   - dict     → {"name": "Faruk", "age": 20}
+
+7. Binary Types
+   - bytes     → b"hello"
+   - bytearray → bytearray([65, 66, 67])
+   - memoryview→ memoryview(b"hello")
+"""
+# Example:
+
+a = 10
+b = 3.14
+c = "Python"
+d = True
+e = [1, 2, 3]
+f = (1, 2, 3)
+g = {"x", "y"}
+h = {"name": "Faruk", "age": 20}
+
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'float'>
+print(type(c))  # <class 'str'>
+print(type(d))  # <class 'bool'>
+print(type(e))  # <class 'list'>
+print(type(f))  # <class 'tuple'>
+print(type(g))  # <class 'set'>
+print(type(h))  # <class 'dict'>
+
