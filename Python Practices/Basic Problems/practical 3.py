cfd5fd12196e60dca6ca8136efe79eb9cf61a8b2
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-09-30 18:36:51 +0600

    Uploaded all files

--- Python Practices/Basic Problems/practical 3.py
+++ Python Practices/Basic Problems/practical 3.py
@@ -6,21 +6,6 @@ def even_odd(n):
 
 print(even_odd(10))
 
-# swap two numbers
-x = 10 # first number
-y = 20 # second number
-x, y = y, x # swap numbers x and y using tuple unpacking
-print("x =", x)
-print("y =", y)
-
-# use extra variables
-x = 10 # first number
-y = 20 # second number
-temp = x # store value of x in temp
-x = y # store value of y in x
-y = temp # store value of temp in y
-print("x =", x)
-print("y =", y)
 
 # find the largest number
 x = 10 # first number
