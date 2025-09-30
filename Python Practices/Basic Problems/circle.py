865ae05adb4b563a524f82cb472be2a192e89f1c
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-09-30 14:56:54 +0600

    Uploaded all files

---
+++ Python Practices/Basic Problems/circle.py
@@ -0,0 +1,46 @@
+def circle_area(radius):
+    return 3.14 * radius * radius
+
+print(circle_area(5))
+
+
+def circle_circumference(radius):
+    return 2 * 3.14 * radius
+
+print(circle_circumference(5))
+
+
+def circle_diameter(radius):
+    return 2 * radius
+
+print(circle_diameter(5))
+
+def circle_radius(radius):
+    return radius
+
+print(circle_radius(5))
+
+def circle_height(radius):
+    return radius
+
+print(circle_height(5))
+
+def circle_volume(radius):
+    return 4/3 * 3.14 * radius * radius * radius
+
+print(circle_volume(5)) 
+
+def circle_surface_area(radius):
+    return 4 * 3.14 * radius * radius
+
+print(circle_surface_area(5))
+
+def circle_surface_volume(radius):
+    return 4/3 * 3.14 * radius * radius * radius
+
+print(circle_surface_volume(5))
+
+def circleOfPerimeter(radius):
+    return 2 * 3.14 * radius
+
+print(circleOfPerimeter(5))
