b54fa2344da6bb55e1e1c9517e54173356df9f55
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-09-30 15:24:22 +0600

    Uploaded all files

---
+++ Python Practices/Basic Problems/math.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("The sum of", num1, "and", num2, "is", num1 + num2)
+
+print("The difference of", num1, "and", num2, "is", num1 - num2)
+
+print("The product of", num1, "and", num2, "is", num1 * num2)
+
+print("The quotient of", num1, "and", num2, "is", num1 / num2)
+
+print("The remainder of", num1, "and", num2, "is", num1 % num2)
+
+print("The power of", num1, "and", num2, "is", num1 ** num2)
+
+print("The floor division of", num1, "and", num2, "is", num1 // num2)
+
+
