fbaa349e7bfe76812dd9d33bced6244b8e997d1d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-01 09:32:09 +0600

    Uploaded all files

---
+++ Python Practices/01_Basics/python keyword/python keyword.py
@@ -0,0 +1,12 @@
+import keyword
+print(keyword.kwlist)
+# output:
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+
+# check if a word is keyword or not
+print(keyword.iskeyword('for'))
+print(keyword.iskeyword('faruk'))
+# output:
+# True
+# False
