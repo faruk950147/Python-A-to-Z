3f073108f8d23a3a9ec40f446474d92aecb6b1bc
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-04 21:39:57 +0600

    I hhave changes

---
+++ Python Methods.py
@@ -0,0 +1,29 @@
+"""
+# 🔹 Built-in Functions
+abs(), all(), any(), ascii(), bin(), bool(), bytearray(), bytes(), callable(), chr(), classmethod(), compile(), complex(), delattr(), dict(), dir(), divmod(), enumerate(), eval(), exec(), filter(), float(), format(), frozenset(), getattr(), globals(), hasattr(), hash(), help(), hex(), id(), input(), int(), isinstance(), issubclass(), iter(), len(), list(), locals(), map(), max(), memoryview(), min(), next(), object(), oct(), open(), ord(), pow(), print(), property(), range(), repr(), reversed(), round(), set(), setattr(), slice(), sorted(), staticmethod(), str(), sum(), super(), tuple(), type(), vars(), zip(), __import__()
+
+# 🔹 String Methods
+capitalize(), casefold(), center(), count(), encode(), endswith(), expandtabs(), find(), format(), format_map(), index(), isalnum(), isalpha(), isascii(), isdecimal(), isdigit(), isidentifier(), islower(), isnumeric(), isprintable(), isspace(), istitle(), isupper(), join(), ljust(), lower(), lstrip(), maketrans(), partition(), removeprefix(), removesuffix(), replace(), rfind(), rindex(), rjust(), rpartition(), rsplit(), rstrip(), split(), splitlines(), startswith(), strip(), swapcase(), title(), translate(), upper(), zfill()
+
+# 🔹 List Methods
+append(), clear(), copy(), count(), extend(), index(), insert(), pop(), remove(), reverse(), sort()
+
+# 🔹 Dictionary Methods
+clear(), copy(), fromkeys(), get(), items(), keys(), pop(), popitem(), setdefault(), update(), values()
+
+# 🔹 Set Methods
+add(), clear(), copy(), difference(), difference_update(), discard(), intersection(), intersection_update(), isdisjoint(), issubset(), issuperset(), pop(), remove(), symmetric_difference(), symmetric_difference_update(), union(), update()
+
+# 🔹 Tuple Methods
+count(), index()
+
+# 🔹 File Object Methods
+close(), detach(), fileno(), flush(), isatty(), read(), readable(), readline(), readlines(), seek(), seekable(), tell(), truncate(), writable(), write(), writelines()
+
+# 🔹 Magic (dunder) Methods
+__init__(), __str__(), __repr__(), __len__(), __getitem__(), __setitem__(), __delitem__(), __iter__(), __next__(), __contains__(), __call__(), __enter__(), __exit__(), __add__(), __sub__(), __mul__(), __truediv__(), __eq__(), __lt__(), __gt__(), __hash__(), __bool__()
+
+# 🔹 Class/Object Related
+@classmethod, @staticmethod, __new__, __del__, __getattr__
+
+"""
