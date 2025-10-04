999fd4990aa330e94eedf1613b1d78cc52d77c11
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-04 12:44:48 +0600

    I have changes if elif else file

---
+++ Python Practices/02_Control_Flow/if_elif_else/if_elif_else.py
@@ -0,0 +1,96 @@
+# ================ if else Simple decision making =================== 
+"""
+             ┌───────────────┐
+             │   Start        │
+             └───────┬───────┘
+                     │
+              ┌──────▼───────┐
+              │  x == 20 ?   │
+              └───┬─────┬────┘
+                  │Yes  │No
+       ┌──────────┘     │
+       │                ▼
+  "x is exactly     ┌──────────────┐
+        20"         │  x > 20 ?    │
+                    └───┬─────┬────┘
+                        │Yes  │No
+          ┌─────────────┘     ▼
+          │              ┌──────────────┐
+    "x is greater        │  x < 20 ?    │
+        than 20"         └───┬─────┬────┘
+                             │Yes  │No
+                 ┌───────────┘     ▼
+                 │             "x is not 20"
+           "x is less than 20"
+
+
+"""
+x = 15
+
+if x == 20:
+    print("x is exactly 20")
+else:
+    print("x is not 20")
+    
+    # ================ if elif else Simple decision making =================== 
+x = 15
+
+if x == 20:
+    print("x is exactly 20")
+elif x > 20:
+    print("x is greater than 20")
+else:
+    print("x is not 20")
+    
+# ================ if elif else advanced decision making =================== 
+x = 15
+
+if x == 20:
+    print("x is exactly 20")
+elif x > 20:
+    print("x is greater than 20")
+elif x < 20:
+    print("x is less than 20")
+else:
+    print("x is not 20")
+    
+    
+#  ================ if elif else nested decision making =================== 
+
+"""
+             ┌───────────────┐
+             │     Start      │
+             └───────┬───────┘
+                     │
+              ┌──────▼────────┐
+              │  x > 10 ?     │
+              └───┬─────┬─────┘
+                  │Yes  │No
+       ┌──────────┘     │
+       │                │
+ "x is greater          │
+   than 10"             │
+       │                │
+       ▼                ▼
+ ┌───────────────┐   "x is 10 or less"
+ │   y > 0 ?     │
+ └───┬─────┬─────┘
+     │Yes  │No
+     │     │
+ "y is    "y is zero
+positive"  or negative"
+
+
+"""
+x = 15
+y = 5
+
+if x > 10:
+    print("x is greater than 10")
+    if y > 0:
+        print("y is positive")
+    else:
+        print("y is zero or negative")
+else:
+    print("x is 10 or less")
+
