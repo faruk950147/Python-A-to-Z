eafa003eb09e26af90e0b562dc4fcab148131fc6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-06 08:31:47 +0600

    Python PDF

--- Python Practices/02_Control_Flow/loops/for travelers.py
+++ Python Practices/02_Control_Flow/loops/for travelers.py
@@ -1,15 +1,19 @@
 fruit = ['apple', 'banana', 'cherry']
 for i in range(len(fruit)):
     print(fruit[i])
+else:
+    print("No more fruits")
     
 # output:
 # apple
 # banana
 # cherry
+# No more fruits
 
 for i in fruit:
     print(i)
-    
+else:
+    print("No more fruits")    
 # output:
 # apple
 # banana
--- Python Practices/02_Control_Flow/loops/while travelers.py
+++ Python Practices/02_Control_Flow/loops/while travelers.py
@@ -3,7 +3,10 @@ i = 0
 while i < len(fruit):
     print(fruit[i])
     i = i + 1
+else:
+    print("No more fruits")
 # output:
 # apple
 # banana
 # cherry
+# No more fruits
