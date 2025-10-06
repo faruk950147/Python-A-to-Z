51e5864267292bfc406011077ad0aae0a9719279
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-06 19:55:26 +0600

    Python PDF

--- Python Practices/03_Data_Structures/Built-in/lists/problems/listOperations.py
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/listOperations.py
@@ -1,8 +1,13 @@
 # Higher order function
+# map() returns a map iterator object (which is an iterator)
+
 def list_sum(list1):
     return list1 + list1
 
 lst = [1, 2, 3, 4, 5]
 sum = list(map(list_sum, lst))
+print(sum)
+
+sum = (map(lambda x: x + x, lst))
 print(sum)
 
