78627f45e4f5b8d0a9d8b6f13b492a6bd4b7385b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-06 08:17:09 +0600

    Python PDF

---
+++ Python Practices/02_Control_Flow/loops/for travelers.py
@@ -0,0 +1,16 @@
+fruit = ['apple', 'banana', 'cherry']
+for i in range(len(fruit)):
+    print(fruit[i])
+    
+# output:
+# apple
+# banana
+# cherry
+
+for i in fruit:
+    print(i)
+    
+# output:
+# apple
+# banana
+# cherry
---
+++ Python Practices/02_Control_Flow/loops/while travelers.py
@@ -0,0 +1,9 @@
+fruit = ['apple', 'banana', 'cherry']
+i = 0
+while i < len(fruit):
+    print(fruit[i])
+    i = i + 1
+# output:
+# apple
+# banana
+# cherry
