14f2f4d2c9addcb4f031ff62254386429f138781
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-06 08:20:15 +0600

    Python PDF

--- Python Practices/02_Control_Flow/break_continue_pass/break_continue_pass.py
+++ Python Practices/02_Control_Flow/break_continue_pass/break_continue_pass.py
@@ -1,6 +1,27 @@
 # break is used to exit the loop
 # continue is used to skip the current iteration
 # pass is used to do nothing
+# ============= break =================== 
+i = 0
+while i < 10:
+    print("I am a loop")
+    i += 1
+    break
+# output:
+# I am a loop
+
+i = 0
+while i < 10:
+    if i == 5:
+        break
+    print(i)
+    i += 1
+# output:
+# 0
+# 1
+# 2
+# 3
+# 4
 
 # ================ break =================== 
 for i in range(10):
