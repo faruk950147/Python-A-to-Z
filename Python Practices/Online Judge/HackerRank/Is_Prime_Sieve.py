78c29ff27522a1d94774ce9606c8360592d874a0
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-07 08:11:24 +0600

    Python PDF

--- Python Practices/Online Judge/HackerRank/Is_Prime_Sieve.py
+++ Python Practices/Online Judge/HackerRank/Is_Prime_Sieve.py
@@ -1,24 +1,24 @@
-def Is_Prime_Sieve(n):
-    if n <= 1:
-        return False
-    elif n <= 3:
-        return True
-    elif (n % 2) == 0 or (n % 3) == 0:
-        return False
-    else:
-        i = 5
-        while (i * i <= n):
-            if (n % i) == 0 or (n % (i + 2)) == 0:
-                return False
-            i += 6
-    return True
+# def Is_Prime_Sieve(n):
+#     if n <= 1:
+#         return False
+#     elif n <= 3:
+#         return True
+#     elif (n % 2) == 0 or (n % 3) == 0:
+#         return False
+#     else:
+#         i = 5
+#         while (i * i <= n):
+#             if (n % i) == 0 or (n % (i + 2)) == 0:
+#                 return False
+#             i += 6
+#     return True
 
-n = int(input())
-for x in range(1, n):
-    if Is_Prime_Sieve(x):
-        print(f'{x} is a prime number')
-    else:
-        print(f'{x} is not a prime number')
+# n = int(input())
+# for x in range(1, n):
+#     if Is_Prime_Sieve(x):
+#         print(f'{x} is a prime number')
+#     else:
+#         print(f'{x} is not a prime number')
         
         
 def Is_Prime_Sieve(n):
@@ -33,3 +33,5 @@ def Is_Prime_Sieve(n):
             if (n % i) == 0 or (n % (i + 2)) == 0:
                 return False
     return True
+
+print(Is_Prime_Sieve(2))
