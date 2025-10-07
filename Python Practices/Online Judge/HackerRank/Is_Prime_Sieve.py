24db2c29df24f9f152ca922a0a37a6bfad56a834
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-07 07:59:56 +0600

    Python PDF

--- Python Practices/Online Judge/HackerRank/Is_Prime_Sieve.py
+++ Python Practices/Online Judge/HackerRank/Is_Prime_Sieve.py
@@ -19,3 +19,17 @@ for x in range(1, n):
         print(f'{x} is a prime number')
     else:
         print(f'{x} is not a prime number')
+        
+        
+def Is_Prime_Sieve(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif (n % 2) == 0 or (n % 3) == 0:
+        return False
+    else:
+        for i in range(5, int(n ** 0.5) + 1, 6):
+            if (n % i) == 0 or (n % (i + 2)) == 0:
+                return False
+    return True
