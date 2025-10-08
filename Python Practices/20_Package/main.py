16241a4727590ff8a8ef13e7a14aa384972712cc
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-08 23:37:25 +0600

    Python PDF

---
+++ Python Practices/20_Package/main.py
@@ -0,0 +1,3 @@
+from mathematics.mathematics import Mathematics
+
+print(Mathematics.addtion(7,7))
---
+++ Python Practices/20_Package/mathematics/mathematics.py
@@ -0,0 +1,11 @@
+class Mathematics:
+    def __init__(self):
+        pass
+    def addtion(self, *args):
+        return sum(args)
+    def subtraction(self, *args):
+        pass
+if __name__ == 'main':
+    math = Mathematics()
+    math.addtion(9,9)
+    print("main")
