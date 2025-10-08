8140d84f8b225a10492ca098174790726454828d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-08 23:56:07 +0600

    Python PDF

--- Python Practices/04_Functions/functions_basics/nested function.py
+++ Python Practices/04_Functions/functions_basics/nested function.py
@@ -1,9 +1,20 @@
 # ============================= What is nested function ==============================
 
-def outer():
-    print("outer")
-    def inner():
-        print("inner")
-    return inner
+# def outer():
+#     print("outer")
+#     def inner():
+#         print("inner")
+#     inner()
+# outer()
 
-outer()
+# fun = outer
+# fun()
+
+def add(a, b):
+    def subtruct(a, b):
+        return a - b
+    # print(subtruct(9,6))
+    return a + b
+print(add(9,6))
+d = add
+print(d(9,7))
