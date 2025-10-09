ff8b34dbde406989d29b24ed7ae56afe680873c2
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-09 14:59:50 +0600

    Python PDF

---
+++ Python Practices/08_OOP/class_object/class/class.py
@@ -0,0 +1,48 @@
+class Cartoon:
+    pass
+
+if __name__ == "__main__":
+    cartoon = Cartoon()
+    print(cartoon)
+    
+class Cartoon:
+    # instance variables
+    def __init__(self, name, age):
+        self.name = name    # instance variable 'name'
+        self.age = age      # instance variable 'age'
+
+    # instance method
+    def display(self):
+        print(self.name, self.age)
+if __name__ == "__main__":
+    cartoon = Cartoon("Cartoon", 10)
+    cartoon.display()
+
+class Cartoon:
+    # class variables
+    name = "Cartoon"    # class variable 'name'
+    age = 10            # class variable 'age'
+
+    # instance method
+    def display(self):
+        print(self.name, self.age)
+if __name__ == "__main__":
+    cartoon = Cartoon("Cartoon", 10)
+    cartoon.display()
+    
+    
+class Cartoon:
+    name = "Cartoon"   # class variable
+    age = 10           # class variable
+
+    def __init__(self, name, age):
+        self.name = name    # instance variable
+        self.age = age      # instance variable
+
+    def display(self):
+        print(self.name, self.age)
+        print(Cartoon.name, Cartoon.age)
+
+if __name__ == "__main__":
+    cartoon = Cartoon("Tom", 5)
+    cartoon.display()
