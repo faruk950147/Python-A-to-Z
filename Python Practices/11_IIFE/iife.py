d6356bd9984db189089a6500c9210feb88764872
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-10 09:32:43 +0600

    Python PDF

---
+++ Python Practices/11_IIFE/iife.py
@@ -0,0 +1,18 @@
+# IIFE (Immediately Invoked Function Expression)
+# It is a function that is executed as soon as it is defined.
+# It is a function that is executed as soon as it is defined.
+# It is a function that is executed as soon as it is defined.
+
+# ================================= IIFE with lambda function =================================
+# Example
+print((lambda x: x + 1)(2))
+
+# Example 2
+print((lambda x, y: x + y)(2, 3))
+
+# ================================= IIFE with def function =================================
+def iife():
+    print("Hello")
+
+iife()
+
---
+++ Python Practices/12_DRY/dry.py
@@ -0,0 +1,6 @@
+# DRY (Don't Repeat Yourself)
+# It is a principle of software development that states that every piece of knowledge or logic should be in one place only.
+# It is a principle of software development that states that every piece of knowledge or logic should be in one place only.
+# It is a principle of software development that states that every piece of knowledge or logic should be in one place only.
+
+# Example
