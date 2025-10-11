fd630ba5649e446ca5df0bc341f93407322e9b76
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-11 14:19:41 +0600

    Python PDF

---
+++ Python Practices/01_Basics/naming_conversation.py
@@ -0,0 +1,7 @@
+# snake_case
+# camelCase
+# PascalCase
+
+# module_name all small letters or under_score
+# class_name start with capital letter
+# function_name or variable_name start with small letter
