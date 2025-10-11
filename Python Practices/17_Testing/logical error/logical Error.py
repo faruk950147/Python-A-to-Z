a218ac093bc5ea52bafe08f0a7422964d421c534
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-11 08:42:36 +0600

    Python PDF

---
+++ Python Practices/17_Testing/logical error/logical Error.py
@@ -0,0 +1,8 @@
+# Logical error version
+def average(a, b):
+    return (a + b) / 3   # it should be (a + b) / 2 because it is average of two numbers
+
+x = 10
+y = 20
+result = average(x, y)
+print("Average:", result)
