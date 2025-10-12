58eb4f1b43105ee8f55b6fa449073522d3f82dd4
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-12 12:10:17 +0600

    Python PDF

--- Python Practices/04_Functions/DRY/dry.py
+++ Python Practices/04_Functions/DRY/dry.py
@@ -2,3 +2,14 @@
 # It is a principle of software development that states that every piece of knowledge or logic should be in one place only.
 
 # Example
+# 1. You can pass a function as an argument
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def call_func(func, value):
+    return func(value)
+
+
+
+print(call_func(greet, "Ahmed"))
