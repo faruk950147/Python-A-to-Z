8ca9a5a4714911166173e62502721da7f8bcb9f6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-13 11:47:12 +0600

    Python PDF

--- Python Practices/04_Functions/functions_basics/important.py
+++ Python Practices/04_Functions/functions_basics/important.py
@@ -8,11 +8,24 @@
 # outer_func()
 
 # ============================= nested function return reference =============================
+# def outer_func():
+#     x = 'local'
+#     def inner_func():
+#         print(x)
+#     return inner_func # here inner_func is returned
+
+# a = outer_func()
+# print(a())
+# print(a.__name__)
+
+# ============================= nested function return reference =============================
 def outer_func():
-    x = 'local'
+    x = 5
     def inner_func():
-        print(x)
+        y = 6
+        return x + y
     return inner_func # here inner_func is returned
 
-outer_func()
-
+a = outer_func()
+print(a())
+print(a.__name__)
