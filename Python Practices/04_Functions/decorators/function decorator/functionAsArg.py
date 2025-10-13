250b39d21e1baf65da61124db5d9b04c2809946a
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-13 13:46:24 +0600

    Python PDF

--- Python Practices/04_Functions/decorators/function decorator/functionAsArg.py
+++ Python Practices/04_Functions/decorators/function decorator/functionAsArg.py
@@ -1,4 +1,5 @@
 # ============================= Function as an argument =========================
+# func = display1 
 
 def decorator1(func):
     func()
@@ -8,6 +9,8 @@ def display1():
 
 decorator1(display1)
 # ============================= Function as an argument =========================
+# func = display2
+# word = "World"
 def decorator2(func, word):
     func(word)
 
