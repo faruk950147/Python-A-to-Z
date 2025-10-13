074769fff16e784170e5d0d5e4675aee0832df18
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-13 11:24:02 +0600

    Python PDF

--- Python Practices/04_Functions/callback/main.py
+++ Python Practices/04_Functions/callback/main.py
@@ -1,7 +1,7 @@
 
 def printName(name, callback):
     print(f"My Name is {name}")
-    callback(21)   # here callback function is age and 21 is the argument
+    callback(21)   # here callback function is called age and 21 is the argument
 
 def printAge(age):
     print(f"My age is {age}")
