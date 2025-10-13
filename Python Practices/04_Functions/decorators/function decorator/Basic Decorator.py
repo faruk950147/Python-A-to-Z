399a42e0b9e2f4913cbc3fae009f35ccac8a1c64
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-13 13:56:38 +0600

    Python PDF

--- Python Practices/04_Functions/decorators/function decorator/Basic Decorator.py
+++ Python Practices/04_Functions/decorators/function decorator/Basic Decorator.py
@@ -8,18 +8,31 @@
 # word = "World"
 # decorator2(say_hello)
 # ============================= Basic Decorator =================================
-def decorator2(func): # <-- decorator function takes ONE argument (the function)
-    def wrapper(word): # <-- real actual wrapper function takes ONE argument (the function)
+def decorator1(func): # <-- decorator function takes ONE argument (the function)
+    def wrapper(): # <-- real actual wrapper function takes ONE argument (the function)
         print("Before function call")
-        func(word)
+        func()
         print("After function call")
     return wrapper
 
-@decorator2
-def say_hello(word):
-    print("Hello", word)
+@decorator1
+def say_hello():
+    print("Hello")
 
-say_hello("World")
+say_hello()
+
+# def decorator2(func): # <-- decorator function takes ONE argument (the function)
+#     def wrapper(word): # <-- real actual wrapper function takes ONE argument (the function)
+#         print("Before function call")
+#         func(word)
+#         print("After function call")
+#     return wrapper
+
+# @decorator2
+# def say_hello(word):
+#     print("Hello", word)
+
+# say_hello("World")
 
 
 
@@ -27,23 +40,23 @@ say_hello("World")
 # ============================= Multiple decorators ============================
 # func = test
 
-def deco1(func):
-    def wrapper():
-        print("Deco1 Before")
-        func()
-        print("Deco1 After")
-    return wrapper
+# def deco1(func):
+#     def wrapper():
+#         print("Deco1 Before")
+#         func()
+#         print("Deco1 After")
+#     return wrapper
 
-def deco2(func):
-    def wrapper():
-        print("Deco2 Before")
-        func()
-        print("Deco2 After")
-    return wrapper
+# def deco2(func):
+#     def wrapper():
+#         print("Deco2 Before")
+#         func()
+#         print("Deco2 After")
+#     return wrapper
 
-@deco1
-@deco2
-def test():
-    print("Function Body")
+# @deco1
+# @deco2
+# def test():
+#     print("Function Body")
 
-test()
+# test()
