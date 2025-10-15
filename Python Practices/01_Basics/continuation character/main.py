c16c4383a474431417c026f4381f74b2fcbf66ea
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-15 08:49:50 +0600

    Python PDF

---
+++ Python Practices/01_Basics/continuation character/main.py
@@ -0,0 +1,21 @@
+# what is continuation character in python?
+# continuation character in python is a backslash (\). 
+
+# Example
+a = 2 + 3 + 4 + 5 + \
+    6 + 7 + 8 + 9 + 10 \
+    + 11 + 12 + 13 + 14 + 15
+print(a)
+
+# another example
+b = (3 * 4) + (5 * 6) + \
+    (7 * 8) + (9 * 10) + \
+    (11 * 12) + (13 * 14) + \
+    (15 * 16)
+print(b)
+
+# another example
+str1 = "Hello, World!"
+str2 = "Hello, " + \
+    "World!"
+print(str1 == str2)
