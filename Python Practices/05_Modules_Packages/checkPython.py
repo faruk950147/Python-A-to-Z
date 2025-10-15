0a02245c51f65dd5f78a16fe03d61803c69a403c
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-15 11:29:40 +0600

    Python PDF

--- Python Practices/05_Modules_Packages/checkPython.py
+++ Python Practices/05_Modules_Packages/checkPython.py
@@ -1,14 +1,14 @@
-# how to check cpython or other python implementation
 import sys
 import platform
 
+# Check Python implementation
 print(f"Using sys implementation: {sys.implementation}")
 print(f"Using platform implementation: {platform.python_implementation()}")
 
-# how to check python version
+# Check Python version
 print(f"Using sys version: {sys.version}")
 print(f"Using platform version: {platform.python_version()}")
 
-# how to check python path
+# Check Python path
 print(f"Using sys path: {sys.path}")
-print(f"Using platform path: {platform.python_path()}")
+print(f"Using sys executable: {sys.executable}")  # optional: Python interpreter path
