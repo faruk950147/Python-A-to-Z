7e2e130826aad1f23d6f549efd87249338be23c8
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-15 12:05:31 +0600

    Python PDF

---
+++ Python Practices/04_Functions/custom function/work_abs.py
@@ -0,0 +1,19 @@
+# abs function return absolute value without negative sign
+
+# int absolute value
+x = 2
+y = 3
+
+print(x) # 2
+print(y) # 3
+
+# float absolute value
+a = 10.5
+b = -10.5
+print(abs(a)) # 10.5
+print(abs(b)) # 10.5
+
+# complex absolute value
+# square root of a2 + b2 = c2  
+z = 2 + 3j
+print(abs(z)) # 3.605551275463989
