9a79c0a0ca8d4bd77329e3ec473b4d4d393c1197
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-16 06:08:09 +0600

    Python PDF

---
+++ Python Practices/01_Basics/variable/exponential.py
@@ -0,0 +1,30 @@
+# =========================== Exponential ===========================
+data = 2e7 # that means 2 * 10^7 2e or 2E is same
+print(data)
+print(type(data))
+
+# =========================== Octal ===========================
+data = 0o12 # that means 1 * 8^1 + 2 * 8^0
+print(data)
+print(type(data))
+
+# =========================== Hexadecimal ===========================
+data = 0x12 # that means 1 * 16^1 + 2 * 16^0
+print(data)
+print(type(data))
+
+# =========================== Boolean ===========================
+data = True
+print(data)
+print(type(data))
+
+# =========================== None ===========================
+data = None
+print(data)
+print(type(data))
+
+
+# =========================== String ===========================
+data = "Hello World"
+print(data)
+print(type(data))
