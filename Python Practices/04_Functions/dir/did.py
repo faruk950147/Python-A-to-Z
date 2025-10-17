a4a314148f601f6726fa16e513027efd562527f6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-17 12:49:47 +0600

    Python PDF

---
+++ Python Practices/04_Functions/dir/did.py
@@ -0,0 +1,8 @@
+# dir() is a built-in Python function that returns a list of all attributes and methods of an object, module, class, or variable.
+
+# 🔍 Purpose:
+
+# You use dir() to see what methods, variables, or attributes are available for a given object.
+dir([object])
+numbers = [1, 2, 3]
+print(dir(numbers))
