d12dd21590a5d03a6a7d81029124ebef6179cbdc
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-17 10:16:40 +0600

    Python PDF

---
+++ Python Practices/01_Basics/continuation character/scape Sequences.py
@@ -0,0 +1,14 @@
+# ================================= What is scape Sequences ====================================
+# Scape Sequences is a symbol that is used to perform scape sequences operations.
+
+# 1. Example
+print("Hello World")
+print("Hello World\n")
+print("Hello World\t")
+print("Hello World\r")
+print("Hello World\b")
+print("Hello World\f")
+print("Hello World\\")
+print("Hello World\'")
+print("Hello World\"")
+
