704c8970e19752dd9bbab27ca55a2ff5d6c86218
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-17 21:34:39 +0600

    Python PDF

---
+++ Python Practices/01_Basics/problem/assignment 01.py
@@ -0,0 +1,60 @@
+# question 1. is python case sensitive? while dealing with variables or identifiers
+
+# answer 
+# "YES it is case sensitive and it is a good practice to use meaningful names"
+# while is reserved keyword for python
+
+# question 2. 
+# which of the following is and invalid identifier in python
+# answer 
+# while because it is a reserved keyword
+
+# question 3. 
+# why is python a dynamically typed language?
+# answer 
+# python is a dynamically typed language because it does not require to declare the data type of a variable while declaring it
+
+# question 4. 
+# why python purpose is to be a general purpose language?
+# answer 
+# python purpose is to be a general purpose language because it can be used for a wide range of applications
+
+# question 5. 
+# python is an interpreted language or a compiled language?
+# answer 
+# python is an interpreted and compiled language
+
+# question 6. 
+# what is the difference between a variable and a constant?
+# answer 
+# a variable is a value that can be changed while a constant is a value that cannot be changed
+
+# question 7. 
+# what is the difference between a variable and a function?
+# answer 
+# a variable is a value that can be changed while a function is a block of code that performs a specific task
+
+# question 8. 
+# what is the difference between a variable and a class?
+# answer 
+# a variable is a value that can be changed while a constant is a value that cannot be changed
+
+# question 9. 
+# what is the difference between a variable and a constant?
+# answer 
+# a variable is a value that can be changed while a constant is a value that cannot be changed
+
+# question 10. 
+# what is the difference between a variable and a constant?
+# answer 
+# a variable is a value that can be changed while a constant is a value that cannot be changed
+
+# question 11. 
+# idle is an integrated development environment (IDE) for python?
+# answer 
+# idle is an integrated development environment (IDE) for python
+
+# question 12. 
+# when the download python official website which compiler is used to compile the python code?
+# answer 
+# when the download python official website which compiler is used to C-compile the python code?
