be865e208f9c38c511178e4a8bfad1a6fb216497
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-18 07:39:30 +0600

    Python PDF

--- Python Practices/01_Basics/problem/assignment 02.py
+++ Python Practices/01_Basics/problem/assignment 02.py
@@ -1,9 +1,14 @@
 # question 1. 
 # what is the printed when the following statement is executed?
-# explain each step
 
-# answer
 p = 100
 q = 200
 
 print(p and q)
+# options
+# a 100
+# b 200
+# c 0
+# d None
+# answer
+# b 200 right operand is printed because it is not zero
