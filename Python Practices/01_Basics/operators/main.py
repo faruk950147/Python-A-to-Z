f34578a77e64c31131c3296d843941fd0627a113
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-18 18:42:05 +0600

    Python PDF

--- Python Practices/01_Basics/operators/main.py
+++ Python Practices/01_Basics/operators/main.py
@@ -1,4 +1,11 @@
 data = [10, 20, 30, 40, 50]
 i = 0
+
+# Traverse loop
 while (i := i + 1) < (n := len(data)):
-    print(data[i])
+    print(f"traverse {i} : {data[i]}")
+
+# Pop loop
+while len(data) > 0:
+    print(f"pop {i} : {data.pop()}")
+    i += 1
