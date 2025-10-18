6367677ef949ca0324c693a17e6d911c6824820a
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-18 08:40:40 +0600

    Python PDF

---
+++ Python Practices/02_Control_Flow/if_elif_else/main.py
@@ -0,0 +1,13 @@
+
+a = 10
+b = 20
+
+if a > b:
+    print(f"{b} is greater than {a}")
+else:
+    print(f"{a} is greater than {b}")
+
+if a < b:
+    print(f"{b} is greater than {a}")
+else:
+    print(f"{a} is greater than {b}")
