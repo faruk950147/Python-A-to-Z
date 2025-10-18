35c9496b5097024edd0c63b1fcdcc7978de05010
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-18 08:08:49 +0600

    Python PDF

---
+++ Python Practices/01_Basics/type of  errors/main.py
@@ -0,0 +1,12 @@
+# ===================== type of errors ===================== 
+
+# 1. Syntax Error
+# 2. Runtime Error
+# 3. Logical Error or semantic error
+
+# ===================== Syntax Error ===================== 
+# syntax error is a type of error that occurs when the code is not written in the correct syntax.
+# ===================== Runtime Error =====================
+# runtime error is a type of error that occurs when the code is executed.
+# ===================== Logical Error =====================
+# logical error is a type of error that occurs when the code is executed but the output is not as expected.
