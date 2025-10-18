ee3855cd1c1d9366f5c2326464d7308992d2773f
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-18 07:59:16 +0600

    Python PDF

--- Python Practices/01_Basics/problem/assignment 02.py
+++ Python Practices/01_Basics/problem/assignment 02.py
@@ -4,11 +4,31 @@
 p = 100
 q = 200
 
-print(p and q)
+# print(p and q) # b 200 right operand is printed because it is not zero
+# print(p or q) # a 100 left operand is printed because it is not zero
+
 # options
 # a 100
 # b 200
 # c 0
 # d None
-# answer
-# b 200 right operand is printed because it is not zero
+
+# question 2. 
+# what is the printed when the following statement is executed?
+
+x = 50
+
+# print(x > 10 and x < 10) # b False because both conditions are not true
+# print(x > 10 and x < 100) # a True because both conditions are true
+
+# options
+# a True
+# b False
+# c 50
+# d 10
+
+# question 3. 
+# what is the printed when the following statement is executed?
+
+print(2*3**3*4) # a 216 operator precedence 
+
