3c620dd5a15c7f00b2e2c3fbf42350365b1ccbc6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-18 07:33:25 +0600

    Python PDF

--- Python Practices/01_Basics/problem/assignment 01.py
+++ Python Practices/01_Basics/problem/assignment 01.py
@@ -3,26 +3,41 @@
 # answer 
 # "YES it is case sensitive and it is a good practice to use meaningful names"
 # while is reserved keyword for python
+# options 
+# a machine dependent   b yes   c no   d none
+
 
 # question 2. 
 # which of the following is and invalid identifier in python
 # answer 
-# while because it is a reserved keyword
+# a while because it is a reserved keyword
+# options 
+# a while   b _name   c name_   d name
+
 
 # question 3. 
 # why is python a dynamically typed language?
 # answer 
 # python is a dynamically typed language because it does not require to declare the data type of a variable while declaring it
+# options 
+# a dynamic   b static   c both   d none
+
 
 # question 4. 
 # why python purpose is to be a general purpose language?
 # answer 
 # python purpose is to be a general purpose language because it can be used for a wide range of applications
+# options 
+# a machine learning   b deep learning   c data science   d ai
+
 
 # question 5. 
 # python is an interpreted language or a compiled language?
 # answer 
 # python is an interpreted and compiled language
+# c
+# options 
+# a compiled   b interpreted   c both   d none
 
 # question 6. 
 # what is the difference between a variable and a constant?
@@ -33,6 +48,7 @@
 # what is the difference between a variable and a function?
 # answer 
 # a variable is a value that can be changed while a function is a block of code that performs a specific task
+
 
 # question 8. 
 # what is the difference between a variable and a class?
---
+++ Python Practices/01_Basics/problem/assignment 02.py
@@ -0,0 +1,9 @@
+# question 1. 
+# what is the printed when the following statement is executed?
+# explain each step
+
+# answer
+p = 100
+q = 200
+
+print(p and q)
