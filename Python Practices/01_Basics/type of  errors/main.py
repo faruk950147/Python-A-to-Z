6e8a2f1686b674833e8f02af3ae428645277210b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-18 08:18:00 +0600

    Python PDF

--- Python Practices/01_Basics/type of  errors/main.py
+++ Python Practices/01_Basics/type of  errors/main.py
@@ -10,3 +10,21 @@
 # runtime error is a type of error that occurs when the code is executed.
 # ===================== Logical Error =====================
 # logical error is a type of error that occurs when the code is executed but the output is not as expected.
+
+
+# ========================= syntax error =========================
+# syntax error is a type of error that occurs when the code is not written in the correct syntax.
+# print("Hello World) # syntax error because of missing closing quotation
+
+# ========================= runtime error =========================
+# runtime error is a type of error that occurs when the code is executed.
+# print(10 / 0) # runtime error because of division by zero
+
+# ========================= logical error =========================
+# logical error is a type of error that occurs when the code is executed but the output is not as expected.
+a = 10
+b = 20
+av = a + b / 3
+# logical error because of we want to calculate the average of a and b but we are adding a and b and then dividing by 3
+print(av) 
+
