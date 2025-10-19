3cc16f783255a1147804dff3730886d72cd294aa
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-19 07:34:32 +0600

    Python PDF

---
+++ Python Practices/01_Basics/datatypes/float.py
@@ -0,0 +1,13 @@
+# =================== Float ===================
+# it has limited length of digits (approximately 15–17 decimal digits)
+# and a value range of about ±1.8 × 10^308
+float = 1.e10 # float    → 1.e10, -5.e-10
+print("float = 1.e10 # float    → 1.e10, -5.e-10\nprint(float) # 10000000000.0")
+float = 1.e-10 # float    → 1.e10, -5.e-10
+print("float = 1.e-10 # float    → 1.e10, -5.e-10\nprint(float) # 0.0000000001")
+float = 3.14 # float    → 3.14, -0.5
+print("float = 3.14 # float    → 3.14, -0.5\nprint(float) # 3.14")
+
+# =================== Float Example ===================
+num_float = 3.14 # float    → 3.14, -0.5
+print(type(num_float)) # <class 'float'>
---
+++ Python Practices/01_Basics/datatypes/integer.py
@@ -0,0 +1,9 @@
+# =================== Integer ===================
+# it has unlimited length of digits only limited by memory 
+# but in other programming languages, numbers have a fixed size.
+int = 10 # int      → 10, -5, 1000
+print(type(int)) # <class 'int'>
+
+# =================== Integer Example ===================
+num_int = 10            # int → 10, -5, 1000
+print(type(num_int))    # <class 'int'>
