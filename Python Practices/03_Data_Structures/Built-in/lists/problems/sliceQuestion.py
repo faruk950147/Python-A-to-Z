04e217abe004952c00ff4329ad7e3038913e6c2d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-19 08:32:29 +0600

    Python PDF

---
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/sliceQuestion.py
@@ -0,0 +1,7 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+# [start:stop:step]
+# start = 0
+# stop = -4
+# step = -1
+# because it will start from the end and go to the 4th element from the end
+print(list[:-4:-1]) # [10,9,8] 
