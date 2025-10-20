f512835c563a4341e54caf18854175c823209680
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-20 19:35:46 +0600

    Python PDF

---
+++ Python Practices/03_Data_Structures/Built-in/sets/problems/subSet.py
@@ -0,0 +1,8 @@
+# ============================= Sub Set =============================
+# Sub Set is a set that contains all elements of another set.
+# Sub Set is denoted by the symbol "⊆".
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print(a.issubset(b)) # False
+print(a <= b) # False
---
+++ Python Practices/03_Data_Structures/Built-in/sets/problems/superSet.py
@@ -0,0 +1,8 @@
+# ============================= Super Set =============================
+# Super Set is a set that contains all elements of another set.
+# Super Set is denoted by the symbol "⊆".
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print(a.issuperset(b)) # False
+print(a >= b) # False
