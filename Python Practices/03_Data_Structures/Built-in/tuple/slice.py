ba123d14366519a0a575df951dc577996d26c46f
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-20 14:46:34 +0600

    Python PDF

---
+++ Python Practices/03_Data_Structures/Built-in/tuple/slice.py
@@ -0,0 +1,45 @@
+# ===================== what is slice =====================
+
+# slice is a built-in function that returns a slice object, 
+# which can be used to extract a portion of a sequence (e.g., a string, list, or tuple).
+
+# slice(start, stop, step)
+
+# slice(start, stop)
+
+# slice(stop)
+
+# ===================== positive and negative index =====================
+
+# positive index is the index of an element in a sequence, starting from 0 for the first element. and stop is the index of the element after the last element to be extracted.
+
+# negative index is the index of an element in a sequence, starting from -1 for the last element. and stop is the index of the element after the last element to be extracted.
+
+# ===================== step =====================
+
+# step is the number of elements to skip between each element to be extracted.
+
+# step can be positive or negative.
+
+# if step is positive, the slice will extract elements from left to right.
+
+# if step is negative, the slice will extract elements from right to left.
+
+# ===================== positive slice =====================
+
+# positive slice is a slice that extracts elements from left to right.
+
+tup = ("h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d")
+
+# tup = ("h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d")
+#        0    1    2    3    4    5    6    7    8    9   10
+#       -11   -10  -9   -8   -7   -6   -5   -4   -3   -2  -1
+print(len(tup))
+
+# ===================== negative slice =====================
+# negative slice is a slice that extracts elements from right to left.
+
+
+# tup = ("h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d")
+#        0    1    2    3    4    5    6    7    8    9   10
+#       -11   -10  -9   -8   -7   -6   -5   -4   -3   -2  -1
