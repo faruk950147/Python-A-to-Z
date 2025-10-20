5528ecf1a4ce2d710292829c8018a1d99bb9fd91
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-20 17:42:51 +0600

    Python PDF

--- Python Practices/03_Data_Structures/Built-in/sets/what is set.py
+++ Python Practices/03_Data_Structures/Built-in/sets/what is set.py
@@ -3,3 +3,13 @@
 # Set is a collection of items. 
 # Set is unordered, unindexed, and unchangeable.
 # Set is written with round brackets.
+# when the run output show unordered, 
+# unindexed, and unchangeable, it means that the set is unordered,
+# unindexed, and unchangeable.
+
+# Creating a set
+set1 = {'h', 'e', 'l', 'l', 'o'}
+set2 = set((1, 2, 3, 4, 5))
+
+print(set1) 
+print(set2)
