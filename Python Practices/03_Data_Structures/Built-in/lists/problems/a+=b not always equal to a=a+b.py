cdadb2072104b86b451aa3b4b6173e5da7587a5d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-20 13:24:50 +0600

    Python PDF

---
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/a+=b not always equal to a=a+b.py
@@ -0,0 +1,29 @@
+# =================== memory address deference ===================
+a = 10
+print('Value of a first line:', a)
+print('ID of a first line:', id(a))
+a = a + 20 # create new object and assign to a
+print('Value of a second line:', a)
+print('ID of a second line:', id(a))
+
+b = 10 # immutable object
+print('Value of b third line:', b)
+print('ID of b third line:', id(b))
+b += 20 # create new object and assign to a
+print('Value of b fourth line:', b)
+print('ID of b fourth line:', id(b))
+
+# =================== memory address deference ===================
+a = [1, 2, 3]
+print('Value of a list:', a)
+print('ID of a list:', id(a))
+
+
+a = a + [4, 5, 6]
+print('Value of a list:', a)
+print('ID of a list:', id(a))
+
+
+a += [4, 5, 6] # append to the same object
+print('Value of a list:', a)
+print('ID of a list:', id(a))
