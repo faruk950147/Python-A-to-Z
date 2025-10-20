ef778c1c03f00158507e41cb590968cf346521f2
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-20 09:11:01 +0600

    Python PDF

---
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/pack.py
@@ -0,0 +1,6 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # this is a pack of cards
+
+print(list)
+# unpack the list
+a, b, c, d, e, f, g, h, i, j = list
+print(a, b, c, d, e, f, g, h, i, j)
---
+++ Python Practices/03_Data_Structures/Built-in/tuple/problems/pack.py
@@ -0,0 +1,6 @@
+tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) # this is a pack of cards
+
+print(tuple)
+# unpack the tuple
+a, b, c, d, e, f, g, h, i, j = tuple
+print(a, b, c, d, e, f, g, h, i, j)
