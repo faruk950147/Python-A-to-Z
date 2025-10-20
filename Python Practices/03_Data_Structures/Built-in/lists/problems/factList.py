14019f97374aa44754638c0014195acbeb2824fc
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-20 08:49:54 +0600

    Python PDF

---
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/factList.py
@@ -0,0 +1,48 @@
+# Using +=
+def factList(n):
+    factList = [1, 1]           # first two values
+    if n <= 2:
+        return factList[:n]     # if input is 2 or less
+    fact = 2                    # 2! = 2
+    for i in range(3, n + 1):   # from 3 to n
+        fact *= i               # previous value * i
+        factList += [fact]      # add to list
+    return factList
+
+print(factList(10))
+
+
+
+# Using append
+def factList(n):
+    factList = [1, 1]           # first two values
+    if n <= 2:
+        return factList[:n]     # if input is 2 or less
+    fact = 2                    # 2! = 2
+    for i in range(3, n + 1):   # from 3 to n
+        fact *= i               # previous value * i
+        factList.append(fact)   # add to list
+    return factList
+
+print(factList(10))
+
+def factList(n):
+    factList = [1]
+    if n == 0:
+        return factList[:1]
+    for i in range(1, n):
+        factList += [factList[-1] * (i + 1)]
+    return factList
+
+print(factList(10))
+
+def factList(n):
+    factList = [1]  # 0! = 1
+    if n == 0:
+        return factList[:1]
+    for i in range(1, n):  # from 1 to n
+        factList.append(factList[-1] * (i + 1))  # last value * (i+1)
+    return factList
+
+print(factList(10))
+
---
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/fibbList.py
@@ -0,0 +1,46 @@
+def fibList(n):
+    fibList = [1, 1]
+    if n <= 2:
+        return fibList[:n]
+    fibPrev, fibCurr = 1, 1
+    for i in range(3, n + 1):
+        fibPrev, fibCurr = fibCurr, fibPrev + fibCurr
+        fibList += [fibCurr]  # inefficient but works
+    return fibList
+
+print(fibList(10))
+
+def fibList(n):
+    fibList = [1, 1]           # first two values
+    if n <= 2:
+        return fibList[:n]     # if input is 2 or less
+    fibPrev, fibCurr = 1, 1    # previous and current values
+    for i in range(3, n + 1):  # from 3 to n
+        fibPrev, fibCurr = fibCurr, fibPrev + fibCurr
+        fibList.append(fibCurr)   # add to list
+    return fibList
+
+print(fibList(10))
+
+def fibList(n):
+    fibList = [1, 1]
+    if n <= 2:
+        return fibList[:n]
+    for i in range(2, n):
+        fibList += [fibList[-1] + fibList[-2]]  # fibList[-1] and fibList[-2] add
+    return fibList
+
+print(fibList(10))
+
+
+# Using append
+def fibList(n):
+    fibList = [1, 1]
+    if n <= 2:
+        return fibList[:n]
+    for i in range(2, n):
+        fibList.append(fibList[-1] + fibList[-2])  # fibList[-1] and fibList[-2] add
+    return fibList
+
+print(fibList(10))
+
