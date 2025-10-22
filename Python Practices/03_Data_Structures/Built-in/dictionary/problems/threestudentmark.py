6583aa7f2e226ddb6bf6fff946f7037e6cb341d8
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-22 08:29:35 +0600

    Python PDF

--- Python Practices/03_Data_Structures/Built-in/dictionary/problems/threestudentmark.py
+++ Python Practices/03_Data_Structures/Built-in/dictionary/problems/threestudentmark.py
@@ -2,8 +2,11 @@ faruk = {"C": 80, "C++": 90, "C#": 70}
 tamim = {"Python": 80, "Java": 90, "JS": 70}
 tonmoy = {"HTML": 80, "CSS": 90, "Bootstrap": 70}
 students = [faruk, tamim, tonmoy]
+lst = []
 for student in students:
     sum1 = 0
     for item in student:
         sum1 += student[item]
-    print(sum1)
+    # print(sum1)
+    lst.append(sum1)
+print(lst)
