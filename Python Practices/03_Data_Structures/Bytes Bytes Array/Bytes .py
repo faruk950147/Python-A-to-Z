ab34acd9bbe3416c602181205c3803d724ffda31
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-22 07:57:16 +0600

    Python PDF

---
+++ Python Practices/03_Data_Structures/Bytes Bytes Array/Bytes .py
@@ -0,0 +1,8 @@
+# ===================== what is bytes ==========================
+# bytes is an immutable sequence of bytes. 
+# all elements of bytes must be in range(0, 256).
+
+# ===================== how to create bytes =====================
+# bytes()
+# bytes.fromhex()
+# bytes.maketrans()
