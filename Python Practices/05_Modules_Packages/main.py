f840cbfb65c3ea1fb12e4f2d40c7cd62a203c01d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-23 22:44:01 +0600

    Python PDF

--- Python Practices/05_Modules_Packages/main.py
+++ Python Practices/05_Modules_Packages/main.py
@@ -2,3 +2,13 @@
 # Python script is a file that contains python code
 
 print("Hello World")
+# command to run python Directly as a script:
+# This runs the Python file directly.
+# Use this for standalone scripts.
+# python main.py
+
+# command to run python Using the -m module flag:
+# This runs the file as a module.
+# Useful when the file is part of a package or you want module-relative imports to work.
+# without extension .py execute 
+# python -m main
