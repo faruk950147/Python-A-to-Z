1d6a4722e350c13853e9699927821aff22e74590
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-25 18:29:35 +0600

    Python PDF

--- Python Practices/03_Data_Structures/Built-in/lists/problems/reduce.py
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/reduce.py
@@ -2,3 +2,34 @@ from functools import reduce
 # reduce() takes two parameters:
 # 1. function
 # 2. iterable
+# ============================= what is reduce? =============================
+# reduce() is a function that takes a function and an iterable as arguments.
+# It applies the function to the first two items in the iterable and then to the result and the next item, and so on.
+# It returns a single value.
+# ============================= example =============================
+def add(x, y):
+    return x + y
+
+print(reduce(add, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+# ============================= working flow of reduce =============================
+# step 1: add(1, 2) → 3
+# step 2: add(3, 3) → 6
+# step 3: add(6, 4) → 10
+# step 4: add(10, 5) → 15
+# step 5: add(15, 6) → 21
+# step 6: add(21, 7) → 28
+# step 7: add(28, 8) → 36
+# step 8: add(36, 9) → 45
+# step 9: add(45, 10) → 55
+
+# 1 + 2 = 3
+# 3 + 3 = 6
+# 6 + 4 = 10
+# 10 + 5 = 15
+# 15 + 6 = 21
+# 21 + 7 = 28
+# 28 + 8 = 36
+# 36 + 9 = 45
+# 45 + 10 = 55
+
