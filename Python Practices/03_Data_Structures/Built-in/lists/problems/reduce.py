3f088c5c13f95a265ebae74394fa2be626c04771
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-25 18:32:02 +0600

    Python PDF

--- Python Practices/03_Data_Structures/Built-in/lists/problems/reduce.py
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/reduce.py
@@ -33,3 +33,18 @@ print(reduce(add, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 # 36 + 9 = 45
 # 45 + 10 = 55
 
+def max(x, y):
+    return x if x > y else y
+
+print(reduce(max, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+# ============================= working flow of reduce =============================
+# step 1: max(1, 2) → 2
+# step 2: max(2, 3) → 3
+# step 3: max(3, 4) → 4
+# step 4: max(4, 5) → 5
+# step 5: max(5, 6) → 6
+# step 6: max(6, 7) → 7
+# step 7: max(7, 8) → 8
+# step 8: max(8, 9) → 9
+# step 9: max(9, 10) → 10
