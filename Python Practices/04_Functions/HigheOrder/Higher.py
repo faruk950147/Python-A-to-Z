113a7dfd95678abe1a60d43109ccbe014f44f0e1
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-25 15:03:33 +0600

    Python PDF

--- Python Practices/04_Functions/HigheOrder/Higher.py
+++ Python Practices/04_Functions/HigheOrder/Higher.py
@@ -1,14 +1,35 @@
 
 # ============================= 1. Function as Argument =======================================
 
-def display(func):
-    print(func(2,3))
+# def display(func):
+#     print(func(2,3))
     
-def add(x, y):
-    return x + y
+# def add(x, y):
+#     return x + y
 
-def mul(x, y):
-    return x * y
+# def mul(x, y):
+#     return x * y
 
-display(add)
-display(mul)
+# display(add)
+# display(mul)
+
+# ============================= 2. Function as Return =======================================
+def display():
+    def add(x, y):
+        return x + y
+    return add
+
+add = display()
+print(add(2, 3))
+
+
+# ============================= 3. Function Higher Order =======================================
+
+def display(func, nums):
+    return func(nums)
+
+def add(nums):
+    return sum(nums)
+
+add = display(add, [2, 3, 4])
+print(add)
