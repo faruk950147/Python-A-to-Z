4f6fa112f516eef7a33629b3153b8172dfaa1ff9
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-26 10:44:42 +0600

    Python PDF

--- Python Practices/08_OOP/class_object/class/class constructor.py
+++ Python Practices/08_OOP/class_object/class/class constructor.py
@@ -11,12 +11,14 @@
 
 # ========================= Example 1: Default Constructor =====================
 class DefaultConstructor:
-    def __init__(self):
-        self.name = "Default User"
-        self.age = 0
-
+    name = "Default User"
+    age = 0
     def showInfo(self):
         print(f"[Default Constructor] Name: {self.name}, Age: {self.age}")
+if __name__ == "__main__":
+    # 1. Default Constructor Object
+    default_obj = DefaultConstructor()
+    default_obj.showInfo()
 
 
 # ========================= Example 2: Parameterized Constructor =====================
@@ -27,6 +29,11 @@ class ParameterizedConstructor:
 
     def showInfo(self):
         print(f"[Parameterized Constructor] Name: {self.name}, Age: {self.age}")
+
+if __name__ == "__main__":
+    # 2. Parameterized Constructor Object
+    param_obj = ParameterizedConstructor("Faruk Ahmed", 22)
+    param_obj.showInfo()
 
 
 # ========================= Example 3: Non-Parameterized Constructor =====================
@@ -39,16 +46,7 @@ class NonParameterizedConstructor:
         print(f"[Non-Parameterized Constructor] Name: {self.name}, Age: {self.age}")
 
 
-# ========================= Object Creation and Output =========================
-
-# 1. Default Constructor Object
-default_obj = DefaultConstructor()
-default_obj.showInfo()
-
-# 2. Parameterized Constructor Object
-param_obj = ParameterizedConstructor("Faruk Ahmed", 22)
-param_obj.showInfo()
-
-# 3. Non-Parameterized Constructor Object
-non_param_obj = NonParameterizedConstructor()
-non_param_obj.showInfo()
+if __name__ == "__main__":
+    # 3. Non-Parameterized Constructor Object
+    non_param_obj = NonParameterizedConstructor()
+    non_param_obj.showInfo()
