6b7d5b13dc87b506bbed71ae6c21b9d7cccbb37c
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-26 23:30:26 +0600

    Python PDF

--- Python Practices/08_OOP/class_object/class/nested class.py
+++ Python Practices/08_OOP/class_object/class/nested class.py
@@ -2,7 +2,6 @@ class University:
     def __init__(self, name):
         self.name = name
         # class of University current object
-        self.department = self.Department("Department")
         
     def display(self):
         print(self.name)
@@ -16,10 +15,35 @@ class University:
 if __name__ == "__main__":
     u = University("TMSS Technical University")
     u.display()
-    u.department.display()
-    u.department1 = u.Department("CSE")
-    u.department2 = u.Department("EEE")
+    department1 = u.Department("CSE")
+    department2 = u.Department("EEE")
 
-    u.department1.display()  # CSE
-    u.department2.display()  # EEE
+    department1.display()  # CSE
+    department2.display()  # EEE
+
+class University:
+    def __init__(self, name):
+        self.name = name
+        self.department = self.Department("Department")  # default department
+        
+    def display(self):
+        print(self.name)
+
+    class Department:
+        def __init__(self, name):
+            self.name = name
+
+        def display(self):
+            print(self.name)
+
+
+if __name__ == "__main__":
+    u = University("TMSS Technical University")
+    u.display()  # prints "TMSS Technical University"
+    u.department.display()  # prints "Department"
+    u.department1 = University.Department("CSE")
+    u.department2 = University.Department("EEE")
+
+    u.department1.display()  # prints "CSE"
+    u.department2.display()  # prints "EEE"
 
