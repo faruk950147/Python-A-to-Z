344d84443066e1df36b683af694ce2ad02a369de
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-26 10:10:41 +0600

    Python PDF

--- Python Practices/08_OOP/class_object/program paradigms.py
+++ Python Practices/08_OOP/class_object/program paradigms.py
@@ -1,5 +1,5 @@
 # ========================= what is program paradigms =========================
-# program paradigms are the different ways of writing a program
+# program paradigms are the different ways of writing a program and organizing the code
 # 1. Procedural Programming
 # 2. Object-Oriented Programming
 # 3. Functional Programming
