a8a9171f298d344da8b009cbcff9a5acd7d9d3f9
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-27 16:45:51 +0600

    Python PDF

--- Python Practices/06_Exception_Handling/raise_custom/main.py
+++ Python Practices/06_Exception_Handling/raise_custom/main.py
@@ -3,12 +3,30 @@
 # You can also create your own "custom exceptions" by defining a new Exception class.
 # This is useful when you want to handle specific errors in your own way.
 
-# Structure:
-# raise Exception("Error message")
-
-# Custom Exception Example:
-# class MyError(Exception):
+# ============================= Custom Exception Syntax =============================
+# class className(Exception):
 #     pass
-# raise MyError("Something went wrong!")
+# raise className("Something went wrong!")
 
-# ============================== basic raise custom =============================
+# ============================= Basic Raise Custom Example =========================
+
+# Step 1: Create a custom exception
+class AgeValueError(Exception):
+    """Raised when something specific goes wrong"""
+    pass
+
+# Step 2: Use the custom exception
+def check_age(age):
+    if age < 18 :
+        # Step 3: Raise the custom exception
+        raise AgeValueError(f"Age is not allowed: {age}")
+    return age
+
+# Step 4: Handle the exception
+try:
+    check_age(15)
+except AgeValueError as e:
+    print(f"AgeValueError: {e}")
+
+# Output:
+# AgeValueError: Age is not allowed: 15
