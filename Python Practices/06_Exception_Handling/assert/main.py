aed76496146efc6415dd5873d5b2ae9a7844fae1
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-28 06:16:08 +0600

    Python PDF

---
+++ Python Practices/06_Exception_Handling/assert/main.py
@@ -0,0 +1,9 @@
+# ====================== what is assert ======================
+# assert is a keyword in Python
+# ====================== syntax =============================
+# assert condition
+# assert condition, message
+# ====================== usage ============================
+# assert is used to testing and debugging mainly used in development 
+# but not used in production
+# assert is used to check preconditions of a function if the condition is false it will raise an AssertionError
