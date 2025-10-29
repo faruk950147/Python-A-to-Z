2f40457ebe6aa33b26e1ee0552d888cb498888c2
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-10-29 18:26:42 +0600

    Python PDF

---
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/any.py
@@ -0,0 +1,5 @@
+# any() takes an iterable and returns True if any element in the iterable is true.
+
+print(any([1, 2, 3, 4, 5]))
+print(any([1, 2, 3, 4, 5, 0]))
+print(any([0, 0, 0, 0, 0]))
---
+++ Python Practices/03_Data_Structures/Built-in/lists/problems/shallowDeep/all.py
@@ -0,0 +1,4 @@
+# all() takes an iterable and returns True if all elements in the iterable are true.
+
+print(all([1, 2, 3, 4, 5]))
+print(all([1, 2, 3, 4, 5, 0]))
