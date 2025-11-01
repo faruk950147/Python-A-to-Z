27f812a71dde31bf8c9f1c84844e906c56119c30
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-01 08:01:02 +0600

    Python PDF

--- Python Practices/09_Advanced/regex/book/basic phone.py
+++ Python Practices/09_Advanced/regex/book/basic phone.py
@@ -1,19 +1,12 @@
 import re
 
-text = "+8801712345678"  #  +919876543210
+text = "+8801712345678"  #: "+919876543210"
 
 # Bangladesh phone pattern
-bd_pattern = r"^(\+?88)?01[3-9]\d{8}$"
+bd_pattern = r"^(\+88)?01[3-9]\d{8}$"
 
 # India phone pattern
-in_pattern = r"^(\+?91)?[6-9]\d{10}$"
-# r is for raw string
-# ^ means start of the string
-# (\+?88)? is for group
-# \+ means + sign 1 or more | ? means optional  
-# [3-9] means any character in the set that is 3 to 9
-# \d{10} means any digit 10 times
-# $ means end of the string
+in_pattern = r"^(\+91)?[6-9]\d{9}$"  #  10 digits (6-9 start)
 
 if re.match(bd_pattern, text):
     if text.startswith("+88"):
@@ -29,4 +22,3 @@ elif re.match(in_pattern, text):
 
 else:
     print("Invalid phone number")
-
