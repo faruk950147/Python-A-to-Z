051708886f888a6b779f0cc24888f4bc226f47b4
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-01 18:08:52 +0600

    I have added php

---
+++ Python Practices/01_Basics/operators/very important.py
@@ -0,0 +1,8 @@
+""" 
+= assignment return a storage value
++= assignment return a storage value
+-= assignment return a storage value
+*= assignment return a storage value
+every operators return a value
+
+"""
