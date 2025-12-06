e0aa4ace91538109f9d7d265438fd0728d8bd957
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-06 13:59:13 +0600

    I have added drf

--- Python Practices/03_Data_Structures/Built-in/string/problems/rev_str.py
+++ Python Practices/03_Data_Structures/Built-in/string/problems/rev_str.py
@@ -1,7 +1,12 @@
 def rev_str(word):
     rev_string = ''
-    for char in word:
-        rev_string = char + rev_string   # every time new char add in front
+    for char in word: 
+        # every time new char add in front
+        rev_string = char + rev_string  
+        # 1 y rev_string = 'y' + '' = y
+        # 2 o rev_string = 'o' + 'y' = oy
+        # 3 u rev_string = 'u' + 'oy' = uoy
+        
         print(f"{char} is char add new rev string in {rev_string}")
     return rev_string
 
