2b555b6dfc7b5291790dc9d6b76fa05684c4efa1
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-06 12:48:21 +0600

    I have added drf

---
+++ Python Practices/04_Functions/function.py
@@ -0,0 +1,12 @@
+# create a function
+# def function_name(parameters):
+#     function body
+#     return value
+
+
+# 1 def is a keyword in python
+# 2 def is used to define a function
+# 3 function_name is the name of the function
+# 4 parameters are the values that are passed to the function or headers of the function ()
+# 5 function body is the code that is executed when the function is called
+# 6 return value is the value that is returned by the function
