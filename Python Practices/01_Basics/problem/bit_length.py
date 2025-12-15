ce7c5bc8e0fa778ab32ae2358fcbf58a6523f1c8
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-15 18:00:40 +0600

    I have added php

--- Python Practices/01_Basics/problem/bit_length.py
+++ Python Practices/01_Basics/problem/bit_length.py
@@ -1,5 +1,5 @@
-def bit_length(num):
-    return num.bit_length()
+def bit_length(bit):
+    return bit.bit_length()
 
 print('bit_length(5):', bit_length(5)) # Output: 3 bits because 5 in binary is 101
 print('bit_length(10):', bit_length(10)) # Output: 4 bits because 10 in binary is 1010
--- Python Practices/01_Basics/problem/count_bit.py
+++ Python Practices/01_Basics/problem/count_bit.py
@@ -1,5 +1,5 @@
-def count_bit(num):
-    return bin(num).count('1')
+def count_bit(bit):
+    return bin(bit).count('1')
 
 print(count_bit(5)) # Output: 2 because 5 in binary is 101
 print(count_bit(10)) # Output: 2 because 10 in binary is 1010
