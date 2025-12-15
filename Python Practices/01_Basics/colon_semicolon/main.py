f7da8db29f7ff8a0f38587f952eaf5c54c2c4764
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-15 14:04:12 +0600

    I have added php

---
+++ Python Practices/01_Basics/colon_semicolon/main.py
@@ -0,0 +1,18 @@
+# Colon in Python
+# It is used to define a block of code
+
+if True:
+    print("True")
+else:
+    print("False")
+
+
+# Semicolon in Python
+# It is used to separate multiple statements on the same line
+
+print("Hello"); print("World"); print("!")
+
+# Better practice (without semicolon)
+print("Hello", end=" ")
+print("World")
+print("!")
