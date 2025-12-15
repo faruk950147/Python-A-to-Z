58270267f0da63c5fcaf23910bb776b7da671b4b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-15 17:46:09 +0600

    I have added php

---
+++ Python Practices/01_Basics/typecasting/basictype.py
@@ -0,0 +1,61 @@
+
+"""
+Example                              | Data Type
+-------------------------------------|-----------
+x = str("Hello World")               | str
+x = int(20)                          | int
+x = float(20.5)                      | float
+x = complex(1j)                      | complex
+x = list(("apple","banana","cherry"))| list
+x = tuple(("apple","banana","cherry"))| tuple
+x = range(6)                         | range
+x = dict(name="John", age=36)        | dict
+x = set(("apple","banana","cherry")) | set
+x = frozenset(("apple","banana","cherry")) | frozenset
+x = bool(5)                          | bool
+x = bytes(5)                         | bytes
+x = bytearray(5)                     | bytearray
+x = memoryview(bytes(5))             | memoryview
+"""
+
+# String
+x = str("Hello World")
+
+# Integer
+x = int(20)
+
+# Float
+x = float(20.5)
+
+# Complex
+x = complex(1j)
+
+# List
+x = list(("apple", "banana", "cherry"))
+
+# Tuple
+x = tuple(("apple", "banana", "cherry"))
+
+# Range
+x = range(6)
+
+# Dictionary
+x = dict(name="John", age=36)
+
+# Set
+x = set(("apple", "banana", "cherry"))
+
+# Frozenset
+x = frozenset(("apple", "banana", "cherry"))
+
+# Boolean
+x = bool(5)
+
+# Bytes
+x = bytes(5)
+
+# Bytearray
+x = bytearray(5)
+
+# Memoryview
+x = memoryview(bytes(5))
