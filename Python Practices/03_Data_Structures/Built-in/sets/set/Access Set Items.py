4522203cf8b5274e8e729728735ba761c33a2082
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-16 11:19:55 +0600

    I have added php

---
+++ Python Practices/03_Data_Structures/Built-in/sets/set/Access Set Items.py
@@ -0,0 +1,30 @@
+# Python - Access Set Items
+# Access Items
+# You cannot access items in a set by referring to an index or a key.
+
+# But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
+
+# ExampleGet your own Python Server
+# Loop through the set, and print the values:
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+# Example
+# Check if "banana" is present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+# Example
+# Check if "banana" is NOT present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" not in thisset)
+
+# Change Items
+# Once a set is created, you cannot change its items, but you can add new items.
---
+++ Python Practices/03_Data_Structures/Built-in/sets/set/Loop Sets.py
@@ -0,0 +1,11 @@
+# Python - Loop Sets
+# Loop Items
+# You can loop through the set items by using a for loop:
+
+# ExampleGet your own Python Server
+# Loop through the set, and print the values:
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
