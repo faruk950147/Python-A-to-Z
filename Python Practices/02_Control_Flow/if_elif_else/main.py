d61e295fb3224729455dfef3b662115697d0d566
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-16 12:43:40 +0600

    I have added php

--- Python Practices/02_Control_Flow/if_elif_else/main.py
+++ Python Practices/02_Control_Flow/if_elif_else/main.py
@@ -1,13 +1,26 @@
+"""
+You think your father three son that mean one father of three son.
+# if condition:
+    # "if condition is true?"
+
+# elif condition:
+    # "elif condition is true?"
+
+# else:
+    # "else condition is true?" 
+    # 
+"""
+
 
 a = 10
 b = 20
 
 if a > b:
-    print(f"{b} is greater than {a}")
-else:
     print(f"{a} is greater than {b}")
+else:
+    print(f"{b} is greater than {a}")
 
 if a < b:
-    print(f"{b} is greater than {a}")
+    print(f"{a} is less than {b}")
 else:
     print(f"{a} is greater than {b}")
