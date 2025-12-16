5a71eb7334e1fdcc93f3681cc50d4513261eb502
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-16 12:49:54 +0600

    I have added php

--- Python Practices/02_Control_Flow/if_elif_else/main.py
+++ Python Practices/02_Control_Flow/if_elif_else/main.py
@@ -1,14 +1,21 @@
 """
 You think your father three son that mean one father of three son.
 # if condition:
+    # The if statement evaluates a condition (an expression that results in True or False). 
+    # If the condition is true, the code block inside the if statement is executed. 
+    # If the condition is false, the code block is skipped.
     # "if condition is true?"
 
 # elif condition:
+    # The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+    # The elif keyword allows you to check multiple expressions for True and execute a block of code as soon as one of the conditions evaluates to True.
     # "elif condition is true?"
 
 # else:
-    # "else condition is true?" 
-    # 
+    # The else keyword is Python's way of saying "if none of the previous conditions were true, then try this condition".
+    # The else keyword is optional and is used to execute a block of code when none of the previous conditions are true.
+    # "else condition is true?"
+    #
 """
 
 
