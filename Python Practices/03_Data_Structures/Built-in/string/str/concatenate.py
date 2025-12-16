d6a712ee0c76e371fc47b4e34404889b8414c557
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-16 09:16:21 +0600

    I have added php

---
+++ Python Practices/03_Data_Structures/Built-in/string/str/concatenate.py
@@ -0,0 +1,19 @@
+# Python - String Concatenation
+# String Concatenation
+# To concatenate, or combine, two strings you can use the + operator.
+
+# ExampleGet your own Python Server
+# Merge variable a with variable b into variable c:
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+# Example
+# To add a space between them, add a " ":
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
