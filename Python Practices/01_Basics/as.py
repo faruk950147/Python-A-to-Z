55c6ab73cacabe101be46f35e4587f3925b52094
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-16 13:12:57 +0600

    I have added php

---
+++ Python Practices/01_Basics/as.py
@@ -0,0 +1,8 @@
+# as is a keyword in python
+# as is used to import a module or a function or a class or a variable
+# rename the imported module or function or class or variable name
+# syntax: import module_name as new_name
+# import math as m
+import math as m
+print(m.sqrt(16))
+print(m.ceil(16.5))
