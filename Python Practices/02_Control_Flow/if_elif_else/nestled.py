7983e2bca4eedb7318a8b70edb1014d3e3593a1b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-16 12:58:27 +0600

    I have added php

--- Python Practices/02_Control_Flow/if_elif_else/nestled.py
+++ Python Practices/02_Control_Flow/if_elif_else/nestled.py
@@ -17,6 +17,19 @@
 #        pass
 # else:
 #     pass
+
+age = 25
+has_license = True
+
+if age >= 18:
+  if has_license:
+    print("You can drive")
+  else:
+    print("You need a license")
+else:
+  print("You are too young to drive")
+
+
 a = 10
 b = 20
 
