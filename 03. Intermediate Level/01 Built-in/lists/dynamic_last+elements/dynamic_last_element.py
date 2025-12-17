fc49c9314ef553d55c1a1a46cab327f0e8ff71f0
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-17 06:45:22 +0600

    I have added php

--- 03. Intermediate Level/01 Built-in/lists/dynamic_last+elements/dynamic_last_element.py
+++ 03. Intermediate Level/01 Built-in/lists/dynamic_last+elements/dynamic_last_element.py
@@ -1,5 +1,12 @@
 def dynamic_last_element(fruits):
     if len(fruits) > 0:
+        # fruits[index] thats mean list of index?
+        # fruits → list
+        # [index] → list index we want which item
+        # len(fruits) - 1 → last index
+        # fruits[len(fruits) - 1] → last item
+        # return fruits[len(fruits) - 1] → return last item
+        # why fruits[len(fruits) - 1]? this inside of dynamic
         return fruits[len(fruits) - 1]
     return None
 
