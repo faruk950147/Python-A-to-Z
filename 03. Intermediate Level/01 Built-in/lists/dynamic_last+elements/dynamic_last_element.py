808f3ca747b90704681e8e4ae1924bc0cf9cc485
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-17 06:22:52 +0600

    I have added php

--- 03. Intermediate Level/01 Built-in/lists/dynamic_last+elements/dynamic_last_element.py
+++ 03. Intermediate Level/01 Built-in/lists/dynamic_last+elements/dynamic_last_element.py
@@ -1,7 +1,12 @@
+def dynamic_last_element(fruits):
+    return fruits[len(fruits) - 1]
+
+
+"""
 fruits = [
     "apple", "banana", "cherry", "Orange"
 ]
 last = fruits[len(fruits) - 1]
 
 
-print(last)
+print(last)"""
