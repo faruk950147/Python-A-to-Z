44eec5f5aba3c3cb01b756b1ecf787f05091e67a
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-21 16:54:17 +0600

    I have added some

--- 03. Intermediate Level/02 Functions/custom function/count.py
+++ 03. Intermediate Level/02 Functions/custom function/count.py
@@ -1,1 +1,6 @@
-def count(char):
+def count_char(ch, text):
+    total = 0
+    for c in range(len(text)):
+        if text[c] == ch:
+            total += 1
+    return total
---
+++ 03. Intermediate Level/02 Functions/custom function/length.py
@@ -0,0 +1,7 @@
+def length(text):
+    count = 0
+    for i in range(len(text)):
+        count += 1
+    return count
+
+print(length("banana"))
