f8eb3d2c927be6af7a96b1d87681a86e4bd779f6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-27 21:28:28 +0600

    I have added some

--- 02. Beginner Level/01 basic concepts/continuation character/scape Sequences.py
+++ 02. Beginner Level/01 basic concepts/continuation character/scape Sequences.py
@@ -8,29 +8,29 @@
 # 1. Example
 # \n is a scape sequence
 str1 = "Hello\nWorld"
-print(f"it is a new line character: {str1}")
+print(f"1. it is a new line character: {str1}")
 # \t is a scape sequence
 str2 = "Hello\tWorld"
-print(f"it is a tab character: {str2}")
+print(f"2. it is a tab character: {str2}")
 # \r is a scape sequence
 str3 = "Hello\rWorld"
-print(f"it is a return character: {str3}")
+print(f"3. it is a return character: {str3}")
 # \b is a scape sequence
 str4 = "Hello\bWorld"
-print(f"it is a backspace character: {str4}")
+print(f"4. it is a backspace character: {str4}")
 # \f is a scape sequence
 str5 = "Hello\fWorld"
-print(f"it is a form feed character: {str5}")
+print(f"5. it is a form feed character: {str5}")
 # \\ is a scape sequence
 str6 = "Hello\\World"
-print(f"it is a backslash character: {str6}")
+print(f"6. it is a backslash character: {str6}")
 # \' is a scape sequence
 str7 = "Hello\'World"
-print(f"it is a single quote character: {str7}")
+print(f"7. it is a single quote character: {str7}")
 # \" is a scape sequence
 str8 = "Hello\"World"
-print(f"it is a double quote character: {str8}")
+print(f"8. it is a double quote character: {str8}")
 # \ is a scape sequence
 str9 = "Hello\\World"
-print(f"it is a backslash character: {str9}")
+print(f"9. it is a backslash character: {str9}")
 
