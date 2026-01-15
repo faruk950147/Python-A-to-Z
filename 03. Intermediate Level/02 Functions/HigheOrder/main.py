897cfc9eff8fe90a9f67fc027a6f5a06aad33a3f
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-01-15 16:47:44 +0600

    I have added some

--- 03. Intermediate Level/02 Functions/HigheOrder/main.py
+++ 03. Intermediate Level/02 Functions/HigheOrder/main.py
@@ -1,3 +1,33 @@
+# Callback Function
+
+# Callback function হলো এমন একটি function,
+# যেটাকে argument হিসেবে অন্য একটি function-এর মধ্যে পাঠানো হয়
+# এবং ওই function-এর ভিতরেই call (execute) করা হয়।
+
+# সহজ ভাষায়
+# একটি function আরেকটি function-কে বলে দেয়—
+# "আমার কাজ শেষ হলে তুমি এই functionটা চালাবে।"
+
+
+# Higher Order Function
+
+# Higher Order Function হলো এমন একটি function,
+# যেটা অন্য একটি function-কে argument হিসেবে নেয়
+# অথবা একটি function return করে।
+
+# সহজ ভাষায়
+# যে function, function নিয়ে কাজ করে
+# সেটাই Higher Order Function।
+
+
+# Callback Function vs Higher Order Function
+
+# | Callback Function                         | Higher Order Function                          |
+# | ----------------------------------------- | ---------------------------------------------- |
+# | যেই function-কে argument হিসেবে পাঠানো হয় | যেই function argument হিসেবে অন্য function নেয় |
+# | পাঠানো function                           | নেওয়া function                                 |
+# | Higher Order function-এর ভিতরে চলে        | Callback function-কে call করে                  |
+# | Example: add()                            | Example: calculate()                           |
 
 from functools import reduce
 
