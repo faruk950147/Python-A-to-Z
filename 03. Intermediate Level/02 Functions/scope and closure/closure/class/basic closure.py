807aede375fed0560faa0f3b6ceee4a32ccc7166
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-01-15 20:48:16 +0600

    I have added some

--- 03. Intermediate Level/02 Functions/scope and closure/closure/class/basic closure.py
+++ 03. Intermediate Level/02 Functions/scope and closure/closure/class/basic closure.py
@@ -1,11 +1,32 @@
-class Multiplier:
+class Calculations:
     def __init__(self, n):
-        self.n = n
+        self.n = n  # Stored value for calculations
 
     def multiply(self, x):
         return x * self.n
 
-double = Multiplier(2)
+    def add(self, x):
+        return x + self.n
 
-print(double.multiply(5))  # 10
-print(double.__dict__)     # Check stored data
+    def subtract(self, x):
+        return x - self.n
+
+    def divide(self, x):
+        if self.n == 0:
+            raise ValueError("Cannot divide by zero!")
+        return x / self.n
+
+    def power(self, x):
+        return x ** self.n
+
+
+# --- Example usage ---
+calc = Calculations(3)
+
+print(calc.multiply(5))  # 15
+print(calc.add(5))       # 8
+print(calc.subtract(5))  # 2
+print(calc.divide(9))    # 3.0
+print(calc.power(2))     # 8
+print(calc.__dict__)     # {'n': 3}
+
