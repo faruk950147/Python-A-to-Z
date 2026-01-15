d07aedd8cd9e76b996c2f3ea3808a9bccd979ee6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-01-15 21:19:50 +0600

    I have added some

--- 03. Intermediate Level/02 Functions/scope and closure/closure/class/basic closure.py
+++ 03. Intermediate Level/02 Functions/scope and closure/closure/class/basic closure.py
@@ -1,6 +1,6 @@
-class Calculations:
+class Calculator:
     def __init__(self, n):
-        self.n = n  # Stored value for calculations
+        self.n = n  # instance variable
 
     def multiply(self, x):
         return x * self.n
@@ -20,13 +20,11 @@ class Calculations:
         return x ** self.n
 
 
-# --- Example usage ---
-calc = Calculations(3)
+calc = Calculator(3)
 
-print(calc.multiply(5))  # 15
-print(calc.add(5))       # 8
-print(calc.subtract(5))  # 2
-print(calc.divide(9))    # 3.0
-print(calc.power(2))     # 8
-print(calc.__dict__)     # {'n': 3}
-
+print(calc.multiply(5))   # 15
+print(calc.add(5))        # 8
+print(calc.subtract(5))   # 2
+print(calc.divide(9))     # 3.0
+print(calc.power(2))      # 8
+print(calc.__dict__)      # {'n': 3}  instance variable check
