25a705384b2722ad4b297165d0a19955c9ab8b9d
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-02-15 14:25:48 +0600

    I have some changes

--- 02. Beginner Level/01 basic concepts/type of  errors/main.py
+++ 02. Beginner Level/01 basic concepts/type of  errors/main.py
@@ -3,15 +3,6 @@
 # 1. Syntax Error
 # 2. Runtime Error
 # 3. Logical Error or semantic error
-
-# ===================== Syntax Error ===================== 
-# syntax error is a type of error that occurs when the code is not written in the correct syntax.
-# ===================== Runtime Error =====================
-# runtime error is a type of error that occurs when the code is executed.
-# ===================== Logical Error =====================
-# logical error is a type of error that occurs when the code is executed but the output is not as expected.
-
-
 # ========================= syntax error =========================
 # syntax error is a type of error that occurs when the code is not written in the correct syntax.
 # print("Hello World) # syntax error because of missing closing quotation
