3c997b959228e1703591351ce1e8c8f4c30992c4
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-02-25 17:03:06 +0600

    I have some changes

--- 07 ML/numpy/main.py
+++ 07 ML/numpy/main.py
@@ -8,6 +8,7 @@ import numpy as np
 
 N = 10000000  # 10 million data points
 
+"""
 # Pure Python implementation
 start_time = time.time()
 print("Pure Python implementation")
@@ -17,6 +18,8 @@ for i in python_array:
     python_sum += i
 print("Python sum:", python_sum)
 print(f"Python time: {time.time() - start_time:.4f} seconds\n")
+"""
+
 
 # NumPy implementation
 start_time = time.time()
@@ -25,3 +28,4 @@ numpy_array = np.arange(N)
 numpy_sum = np.sum(numpy_array)
 print("NumPy sum:", numpy_sum)
 print(f"NumPy time: {time.time() - start_time:.4f} seconds")
+
