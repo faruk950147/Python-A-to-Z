0d51638005aa5ca4aab5e49914fb8478278fa8c8
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-02-26 19:44:27 +0600

    I have some changes

--- 07 ML/numpy/transpose.py
+++
@@ -1,9 +0,0 @@
-# traspose matrix is when we flip the matrix over its diagonal
-import numpy as np
-
-matrix = np.array([[1, 2, 3], [4, 5, 6]])
-print("Original matrix:")
-print(matrix)
-
-print("Transposed matrix:")
-print(matrix.transpose())
