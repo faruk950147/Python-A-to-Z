ad566cc60d0d0d7e239e1c8c7ef3df0852f6425a
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-03-25 12:45:35 +0600

    I have some changes

--- 04. Advanced Level/06 Concurrency/Multithreading/main.py
+++ 04. Advanced Level/06 Concurrency/Multithreading/main.py
@@ -1,5 +1,26 @@
-# 
-
-
 import threading
 import time
+
+def first_program():
+    for i in range(5):
+        print("First Program:", i)
+        time.sleep(1)
+
+def second_program():
+    for i in range(5):
+        print("Second Program:", i)
+        time.sleep(1)
+
+# Thread তৈরি
+t1 = threading.Thread(target=first_program)
+t2 = threading.Thread(target=second_program)
+
+# Thread start
+t1.start()
+t2.start()
+
+# Main thread wait 
+t1.join()
+t2.join()
+
+print("Program Finished")
