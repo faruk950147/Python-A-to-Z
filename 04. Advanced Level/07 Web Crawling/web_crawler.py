de23df0f67aa23420499c14bd9862f65ea1ff064
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-07 18:31:48 +0600

    I have some changes

---
+++ 04. Advanced Level/07 Web Crawling/web_crawler.py
@@ -0,0 +1,38 @@
+class Crawler:
+    def __init__(self, url):
+        self.url = url
+        
+    def create_folder(self):
+    
+        # url: https://www.geeksforgeeks.org/
+        print(self.url)
+        # url: ['https:', '', 'www.geeksforgeeks.org', '']
+        
+        # split the url by //
+        print(self.url.split("//"))
+        # url: ['https:', '', 'www.geeksforgeeks.org', '']
+        
+        # get the last part of the url
+        folder_name = self.url.split("//")[-1].split("/")[0]
+        # folder_name: www.geeksforgeeks.org
+        
+        # create the folder
+        path = os.path.join(
+            "L:\\Programming\\Programming\\All-Main-Problem-Solving\\Python-A-to-Z\\04. Advanced Level",
+            "07 Web Crawling\\websites",
+            folder_name
+        )
+        
+        os.makedirs(path, exist_ok=True)  # already handles exists
+        
+        return path
+
+    def crawl(self):
+        folder_path = self.create_folder()
+        response = requests.get(self.url)
+        with open(os.path.join(folder_path, "index.html"), "w", encoding="utf-8") as f:
+            f.write(response.text)
+        return response.text
+
+crawler = Crawler("https://www.python.org/")
+print(crawler.crawl())
