d7c3d6f4ee3df12accac3e35802b9df20d74facd
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-07 18:08:26 +0600

    I have some changes

--- 04. Advanced Level/07 Web Crawling/main.py
+++ 04. Advanced Level/07 Web Crawling/main.py
@@ -4,10 +4,8 @@ import requests
 
 response = requests.get("https://www.geeksforgeeks.org/")
 
-print("Status Code:", response.status_code)
-
-print("\nResponse Content:")
-print(response.text)
+# check the status code
+print("Status Code:", response.ok)
 
 
 '''
