a6fd8517b27c6649b372f401c000c8e029586503
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-08 18:10:48 +0600

    I have some changes

--- 04. Advanced Level/08 Web scrapping/web_scrapping.py
+++ 04. Advanced Level/08 Web scrapping/web_scrapping.py
@@ -17,12 +17,11 @@ with open('text.html', 'w', encoding='utf-8') as f:
     
 import requests
 from bs4 import BeautifulSoup
-
-response = requests.get('https://www.wscubetech.com/')
-
-soup = BeautifulSoup(response.text, 'html.parser')
+res = requests.get('https://www.youtube.com/watch?v=VIksvMzIS0g')
+# print("Without beautyful soup" + res.content)
+soup = BeautifulSoup(res.content, 'html.parser')
 
 # create a file
 with open('text.html', 'w', encoding='utf-8') as f:
-    f.write(soup.get_text())
+    f.write(soup.prettify())
 
