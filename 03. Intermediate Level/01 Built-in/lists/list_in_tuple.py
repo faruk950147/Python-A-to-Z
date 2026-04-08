8fa61fdc8e35c294a1da99a6bb5995e8336681f8
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-08 14:38:07 +0600

    I have some changes

---
+++ 03. Intermediate Level/01 Built-in/lists/list_in_tuple.py
@@ -0,0 +1,4 @@
+lst = [(1, 2, 3, 4, 5)]
+
+print(lst[0])
+print(lst[0][0])
--- 04. Advanced Level/07 Web Crawling/basic_web_crawler.py
+++ 04. Advanced Level/07 Web Crawling/basic_web_crawler.py
@@ -5,7 +5,11 @@ from urllib.parse import urljoin
 visited = set()
 start_url = "https://books.toscrape.com/"
 
-depth = 2  # how many levels to crawl
+depth = 2
 queue = [(start_url, depth)] 
-
-print(queue[0])
+print(queue[0][0])
+print(queue[0][1])
+while queue:
+    url, current_depth = queue.pop(0)
+    # print(url)
+    # print(current_depth)
