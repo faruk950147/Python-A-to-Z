16765b5820f3b98df0ead6dc0ba83e70cfd00482
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-08 18:06:22 +0600

    I have some changes

--- 04. Advanced Level/08 Web scrapping/web_scrapping.py
+++ 04. Advanced Level/08 Web scrapping/web_scrapping.py
@@ -1,3 +1,4 @@
+""" 
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,11 +10,11 @@ print(response.text)
 # Save to file
 with open('text.html', 'w', encoding='utf-8') as f:
     f.write(response.text)
+
+"""
     
     
     
-    
-'''
 import requests
 from bs4 import BeautifulSoup
 
@@ -21,10 +22,7 @@ response = requests.get('https://www.wscubetech.com/')
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# just text find out (HTML tag)
-print(soup.get_text())
-
-with open('text.txt', 'w', encoding='utf-8') as f:
+# create a file
+with open('text.html', 'w', encoding='utf-8') as f:
     f.write(soup.get_text())
 
-'''
