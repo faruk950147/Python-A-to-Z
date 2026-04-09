47706a10bc5b55f244f33dd17d68fb59bda17029
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-09 14:02:56 +0600

    I have some changes

--- 04. Advanced Level/08 Web scrapping/web_scrapping.py
+++ 04. Advanced Level/08 Web scrapping/web_scrapping.py
@@ -1,20 +1,3 @@
-""" 
-import requests
-from bs4 import BeautifulSoup
-
-response = requests.get('https://www.wscubetech.com/')
-
-# HTML content
-print(response.text)
-
-# Save to file
-with open('text.html', 'w', encoding='utf-8') as f:
-    f.write(response.text)
-
-"""
-    
-    
-    
 import requests
 from bs4 import BeautifulSoup
 res = requests.get('https://www.youtube.com/watch?v=VIksvMzIS0g')
