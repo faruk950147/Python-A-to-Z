4703438ba7e2aa81ee9e52e787b9b9e5f9d6783a
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-09 20:05:56 +0600

    I have some changes

--- 04. Advanced Level/09 Automation/automation.py
+++ 04. Advanced Level/09 Automation/automation.py
@@ -1,3 +1,16 @@
 import os
 import shutil
 
+"""
+| Task                   | Library                     | Example                  |
+| ---------------------- | --------------------------- | ------------------------ |
+| File/Folder operations | `os`, `shutil`              | move, copy, delete       |
+| Web Automation         | `selenium`                  | auto login, form fill    |
+| Data process           | `pandas`                    | Excel, CSV process       |
+| Web Scraping           | `requests`, `BeautifulSoup` | website data collect     |
+| GUI Automation         | `pyautogui`                 | mouse & keyboard control |
+
+"""
+
+
+shutil.copy("source.txt", "destination.txt")
