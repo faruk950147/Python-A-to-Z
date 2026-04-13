206d112d4f421da543296f8775a67ce4f2cc2c6b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-13 21:05:07 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/dictionary/problems/marks.py
+++ 03. Intermediate Level/01 Built-in/dictionary/problems/marks.py
@@ -27,21 +27,21 @@
 #     length = len(studentsMarks[student]["details"]["marks"])
 #     percentage = sum(studentsMarks[student]["details"]["marks"].values()) / length
 #     print(f"{student} : {percentage:.2f} %")
-    
-# def calculate_percentages(students):
-#     for student in students:
-#         marks = students[student]["details"]["marks"]
-#         length = len(marks)
-#         percentage = sum(marks.values()) / length
-#         print(f"{student} : {percentage:.2f} %")
+''' 
+def calculate_percentages(students):
+    for student in students:
+        marks = students[student]["details"]["marks"]
+        length = len(marks)
+        percentage = sum(marks.values()) / length
+        print(f"{student} : {percentage:.2f} %")
 
-# studentsMarks = {
-#     "faruk": {"details": {"roll": 101, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
-#     "tom": {"details": {"roll": 102, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
-#     "jerry": {"details": {"roll": 103, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}
-# }
+studentsMarks = {
+    "faruk": {"details": {"roll": 101, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
+    "tom": {"details": {"roll": 102, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
+    "jerry": {"details": {"roll": 103, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}
+}
 
-# calculate_percentages(studentsMarks)
+calculate_percentages(studentsMarks)
 
 
 class Student:
@@ -63,3 +63,7 @@ students = [Student(name, data["details"]) for name, data in studentsMarks.items
 
 for student in students:
     print(f"{student.name} : {student.percentage():.2f} %")
+    
+    
+'''
+
