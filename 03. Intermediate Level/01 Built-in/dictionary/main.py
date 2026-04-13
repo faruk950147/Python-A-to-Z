d5dec93388462475fe0c1690a34588a532e5e817
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-13 13:41:49 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/dictionary/main.py
+++ 03. Intermediate Level/01 Built-in/dictionary/main.py
@@ -1,4 +1,24 @@
 dict1 = {"name": "Faruk", "age": 22, "city": "Dhaka"}
 
+# dict2 = dict(name="John", age=30, city="Dhaka")
 
+# dict3 = dict([("name", "John"), ("age", 30), ("city", "Dhaka")])
 
+# Accrss
+# print(dict1)
+# print(dict1["name"])
+
+# modify
+# dict1["name"]="Omar Faruk"
+# print(dict1["name"])
+
+# add
+# dict1["Country"]="Bangladesh"
+# print(dict1)
+
+# print(dict1.get("country", "N/A"))
+# print(dict1.keys())
+# print(dict1.items())
+# print(dict1.values())
+# dict1.update({"Country": "Asia"})
+# print(dict1)
