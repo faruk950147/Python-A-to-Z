90f50228f1683f45b608fa3295a0eac2b78f2757
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-13 19:01:29 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/dictionary/problems/threestudentmark.py
+++ 03. Intermediate Level/01 Built-in/dictionary/problems/threestudentmark.py
@@ -2,6 +2,7 @@ faruk = {"C": 80, "C++": 90, "C#": 70}
 tamim = {"Python": 80, "Java": 90, "JS": 70}
 tonmoy = {"HTML": 80, "CSS": 90, "Bootstrap": 70}
 students = [faruk, tamim, tonmoy]
+print(f"Students: {students}")
 lst = []
 for student in students:
     sum1 = 0
