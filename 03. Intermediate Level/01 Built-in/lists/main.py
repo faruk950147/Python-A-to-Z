687eb5275be17d298ea951bcc87070ef574caad3
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-14 08:32:41 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/lists/main.py
+++ 03. Intermediate Level/01 Built-in/lists/main.py
@@ -4,6 +4,8 @@ list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # print(list1[:len(list1)])
 # print(list1[:])
 
-print(list1[-1])    
-print(list1[-2:])    
-print(list1[:-2])    
+# print(list1[-1])    
+# print(list1[-2:])    
+# print(list1[:-2])    
+
+print(any(x > 3 for x in list1))
