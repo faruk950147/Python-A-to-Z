092495aa2860938d6681dbfbeec51b3733066487
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-14 09:44:55 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/lists/list/access_list_items.py
+++ 03. Intermediate Level/01 Built-in/lists/list/access_list_items.py
@@ -2,7 +2,7 @@
 # Access Items
 # List items are indexed and you can access them by referring to the index number:
 
-# ExampleGet your own Python Server
+# Example Get your own Python Server
 # Print the second item of the list:
 
 thislist = ["apple", "banana", "cherry"]
