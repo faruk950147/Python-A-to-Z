480cbc635ba7a65097a179e50825c3bed810aa67
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-14 11:09:11 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/lists/problems/append.py
+++ 03. Intermediate Level/01 Built-in/lists/problems/append.py
@@ -10,3 +10,23 @@ def append_2list(list1, list2):
     return list1
 
 print(append_2list([1, 2, 3], [4, 5, 6]))
+
+
+
+def append_item(list1=None, item=None):
+    if list1 is None:
+        list1 = []
+    list1.append(item)
+    return list1
+
+print(append_item([1, 2, 3], 4))
+
+def append_2list(list1=None, list2=None):
+    if list1 is None:
+        list1 = []
+    if list2 is None:
+        list2 = []
+    list1.append(list2)
+    return list1
+
+print(append_2list([1, 2, 3], [4, 5, 6]))
