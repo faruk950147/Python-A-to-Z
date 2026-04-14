ebf42b3bfea6cf0f9baa8ad2fb184071e5e8a13f
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-14 13:08:18 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/lists/problems/factList.py
+++ 03. Intermediate Level/01 Built-in/lists/problems/factList.py
@@ -9,7 +9,7 @@ def factList(n):
         factList += [fact]      # add to list
     return factList
 
-print(factList(10))
+print(factList(5))
 
 '''
 
@@ -20,6 +20,9 @@ def factList(n):
         return factList[:n]     # if input is 2 or less
     fact = 2                    # 2! = 2
     for i in range(3, n + 1):   # from 3 to n
+        # 2 → 2 * 3 = 6
+        # 6 → 6 * 4 = 24
+        # 24 → 24 * 5 = 120
         fact *= i               # previous value * i
         factList.append(fact)   # add to list
     return factList
@@ -31,6 +34,10 @@ def factList(n):
     if n == 0:
         return factList[:1]
     for i in range(1, n):
+        # 1 → 1 * 2 = 2
+        # 2 → 2 * 3 = 6
+        # 6 → 6 * 4 = 24
+        # 24 → 24 * 5 = 120
         factList += [factList[-1] * (i + 1)]
     return factList
 
