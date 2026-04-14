1fcf335be82c46f13ce3ee924bac8f3ef9f791b2
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-14 10:44:33 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/lists/problems/a+=b not always equal to a=a+b.py
+++ 03. Intermediate Level/01 Built-in/lists/problems/a+=b not always equal to a=a+b.py
@@ -3,29 +3,35 @@
 a = 10
 print('Value of a first line:', a)
 print('ID of a first line:', id(a))
-a = a + 20 # create new object and assign to a
+
+# create new object and assign to a
+a = a + 20  
 print('Value of a second line:', a)
 print('ID of a second line:', id(a))
 
-b = 10 # immutable object
+# create new object and assign to b
+# immutable object
+b = 10  
 print('Value of b third line:', b)
 print('ID of b third line:', id(b))
-b += 20 # create new object and assign to a
+
+# create new object and assign to b
+# immutable object
+b += 20  
 print('Value of b fourth line:', b)
 print('ID of b fourth line:', id(b))
 
-# =================== memory address deference ===================
-# always create new object and assign to a
+# =================== list (mutable vs immutable behavior) ===================
 a = [1, 2, 3]
 print('Value of a list:', a)
 print('ID of a list:', id(a))
 
 # always create new object and assign to a
-a = a + [4, 5, 6]
+a = a + [4, 5, 6]  
 print('Value of a list:', a)
 print('ID of a list:', id(a))
 
-# always create new object and assign to a
-a += [4, 5, 6] # append to the same object
+# modify same object (mutable behavior)
+a += [4, 5, 6]
 print('Value of a list:', a)
 print('ID of a list:', id(a))
