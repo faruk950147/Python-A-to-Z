08751a6c283e4b7331808385d1ecd4e74f06cd77
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-14 18:19:08 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/string/problems/vowelRemover.py
+++ 03. Intermediate Level/01 Built-in/string/problems/vowelRemover.py
@@ -4,8 +4,10 @@ def remove_vowel(word):
         if word[i] in vowels:
             word = word.replace(word[i], "")
     return word
+print(remove_vowel("Hello World"))
 
 import re
 
 def remove_vowel(word):
     return re.sub(r"[aeiouAEIOU]", "", word)
+print(remove_vowel("Hello World"))
