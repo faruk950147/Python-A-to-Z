f73a3c4a612e1a18b0db1f8f6250c163169980a7
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-14 18:28:01 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/string/problems/vowelRemover.py
+++ 03. Intermediate Level/01 Built-in/string/problems/vowelRemover.py
@@ -15,6 +15,16 @@ def remove_vowel(word):
 print(remove_vowel("Hello World"))
 
 
+'''
+maketrans() creates a translation table that maps each character to be replaced 
+to its replacement character. 
+str.maketrans() returns a mapping table that can be used 
+with the translate() method to replace specified characters.
+
+translate() method returns a string where some specified characters are replaced 
+with the character described in a dictionary, or in a mapping table.
+'''
+
 def remove_vowel(word):
     return word.translate(str.maketrans('', '', 'aeiouAEIOU'))
 
