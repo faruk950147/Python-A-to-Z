ceea8c2786044f2d636b1987c9bd7a9eb0ec8d73
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-15 20:25:21 +0600

    I have some changes

--- 04. Advanced Level/02 OOP/abstraction/abstraction.py
+++ 04. Advanced Level/02 OOP/abstraction/abstraction.py
@@ -1,6 +1,8 @@
 # ====================== What is Abstraction ======================
 
-# Abstraction is the process of hiding the implementation details of a class and showing only the necessary details to the user. It is a way to reduce the complexity of a program and make it easier to understand and maintain.
+# Abstraction is the process of hiding the implementation details of a class and showing only 
+# the necessary details to the user. 
+# It is a way to reduce the complexity of a program and make it easier to understand and maintain.
 
 # ====================== Built-in abstract base class ======================
 
