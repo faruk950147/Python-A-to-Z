065887942fc02106e52110e5b7d8f50ba7db0229
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-04-26 10:53:28 +0600

    I have some changes

--- 02. Beginner Level/01 basic concepts/operators/Ternary operator.py
+++ 02. Beginner Level/01 basic concepts/operators/Ternary operator.py
@@ -3,11 +3,9 @@
 # Ternary operator in python does not exist but we can use it by using if-else
 # Ternary operators is a symbol that is used to perform ternary operations.
 
-# condition_if_true if condition else condition_if_false
+# # Syntax:
+# value_if_true if condition else value_if_false
 
-# The syntax for Python’s ternary operator (one-line if-else) is:
-
-# result = value_if_true if condition else value_if_false
 
 books = ["Python", "Django", "Flask", "FastAPI", "React"]
 
