668f175b277cf121bae5ae745866211652ce6bf4
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-05-10 07:20:33 +0600

    I have some changes

--- 05 Problems Solving/main.py
+++ 05 Problems Solving/main.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 '''
 # total_seconds = now.timestamp()
 total_seconds = (now.hour * 3600 + now.minute * 60 + now.second)
@@ -9,14 +9,26 @@ print(f"Total seconds: {int(total_seconds)}")
 print(f"Total minutes: {int(minutes)}")
 print(f"Total hours: {int(hours)}")
 print(f"Total days: {int(days)}")
-'''
+
 
 ONLINE_TIMEOUT_SECONDS = 60  # 60 seconds
 
 last_seen = datetime(2025, 10, 14, 12, 0, 0)
 
-is_online = (
-    datetime.now() - last_seen
-).total_seconds() < ONLINE_TIMEOUT_SECONDS
+is_online = (datetime.now() - last_seen).total_seconds() < ONLINE_TIMEOUT_SECONDS
 
 print(f"Is online: {is_online}")
+'''
+ONLINE_TIMEOUT_SECONDS = 60
+
+# 30 seconds ago
+last_seen = datetime.now() - timedelta(seconds=30)
+print(f"Last seen: {last_seen}")
+is_online = (datetime.now() - last_seen).total_seconds() < ONLINE_TIMEOUT_SECONDS
+print(f"Is online: {is_online}")
+
+diff = (datetime.now() - last_seen).total_seconds()
+print(f"Diff: {diff}")
+print(f"Is online: {diff < ONLINE_TIMEOUT_SECONDS}")
+
+
