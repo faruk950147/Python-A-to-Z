44cbcafcc69408147c231e1336d01df14bc5ddc8
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-05-14 06:31:15 +0600

    I have some changes/

--- 05 Problems Solving/Basic Problems/Celsius.py
+++ 05 Problems Solving/Basic Problems/Celsius.py
@@ -3,8 +3,10 @@ def fahrenheit_to_celsius(fahrenheit):
     """
     Convert Fahrenheit to Celsius
     Formula: (F - 32) * 5/9
+    100 - 32 = 68 * 5/9 = 37.78
     """
     return (fahrenheit - 32) * 5/9
 
 # Test the function
-print(fahrenheit_to_celsius(32))  # Expected: 0.0 (freezing point of water)
+print(fahrenheit_to_celsius(90))  # Expected: 32.22
+print(fahrenheit_to_celsius(100))  # Expected: 37.78
