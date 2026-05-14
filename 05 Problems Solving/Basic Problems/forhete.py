58a4268611510eb86a1668468ed8461c0ae1d018
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-05-14 14:27:24 +0600

    I have some changes/

--- 05 Problems Solving/Basic Problems/forhete.py
+++
@@ -1,6 +0,0 @@
-# Celsius to Fahrenheit
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
-print(celsius_to_fahrenheit(0))
-
-
---
+++ 05 Problems Solving/Basic Problems/root square equation.py
@@ -0,0 +1,4 @@
+def root_square_equation(a, b, c):
+    return (-b + (b**2 - 4*a*c)**0.5) / (2*a)
+
+print(root_square_equation(1, -5, 6))
