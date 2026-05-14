0f4f57a6c399c602fcd43dd81667a62773964bf0
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-05-14 06:15:41 +0600

    I have some changes/

--- 05 Problems Solving/52 problems solving/3.print Tables.py
+++ 05 Problems Solving/52 problems solving/3.print Tables.py
@@ -7,13 +7,14 @@ for num in range(1000, 0, -2):  # 1000 to 2 (odd numbers)
         print()
    
    
-        
-N = int(input("Enter a number (N): "))  # user input
+'''      
+n = int(input("Enter a number (n): "))  # user input
 count = 0  # count for 5 numbers per line
-# N to 2 (odd numbers)
-for num in range(N if N % 2 == 0 else N - 1, 1, -2):  
+# n to 2 (odd numbers)
+for num in range(n if n % 2 == 0 else n - 1, 1, -2):  
     print(num, end="\t")  # print number with tab
     count += 1
     
     if count % 5 == 0:  # print new line after 5 numbers
         print()
+'''
