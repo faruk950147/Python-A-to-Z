8ae8547d7c4410917daa8835bae31550f015ff12
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-05-15 11:09:03 +0600

    I have some changes/

--- 05 Problems Solving/main problem/Rolling Dice.py
+++ 05 Problems Solving/main problem/Rolling Dice.py
@@ -1,11 +1,18 @@
+'''
+what is rolling dice?
+- rolling dice is a process of rolling dice and getting a random number between 1 and 6
+
+'''
 import random
 
-min_value = 1
-max_value = 6
+def roll_dice(min_value=1, max_value=6):
+    return random.randint(min_value, max_value)
 
-roll_again = ""
-
-while roll_again.lower() != "n":
-    print("Rolling the dice...")
-    print("The value is", random.randint(min_value, max_value))
-    roll_again = input("Do you want to roll again? (y/n): ")
+if __name__ == "__main__":
+    while True:
+        user_input = input("Do you want to roll the dice? (y/n): ")
+        if user_input == "y":
+            print(roll_dice(1, 6))
+        else:
+            print("Goodbye!")
+            break
