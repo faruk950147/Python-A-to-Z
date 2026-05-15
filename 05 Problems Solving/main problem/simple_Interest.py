00a2963c72fc7f8a60c85f70780b9e5edaa11970
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-05-15 10:49:12 +0600

    I have some changes/

--- 05 Problems Solving/main problem/simple_Interest.py
+++ 05 Problems Solving/main problem/simple_Interest.py
@@ -1,7 +1,18 @@
 def simpleInterest(principal, rate, time):
-    # principal is the amount of money
-    # rate is the interest rate
-    # time is the time in years
+    """
+    Calculate simple interest.
+    
+    Args:
+        principal (float): Principal amount.
+        rate (float): Interest rate.
+        time (float): Time in years.
+        
+        logic: (principal * rate * time) / 100
+        100 * 10 * 1 / 100 = 10
+        
+    Returns:
+        float: Simple interest.
+    """
     return (principal * rate * time) / 100
 
-print(simpleInterest(100, 10, 1))
+print(simpleInterest(100, 10, 1)) # output: 10.0
