a76255b80807d70f5cc3e31d113538bdeb13c9a9
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-05-15 11:14:31 +0600

    I have some changes/

--- 05 Problems Solving/main problem/Prime checker and star pattern printer.py
+++ 05 Problems Solving/main problem/Prime checker and star pattern printer.py
@@ -14,35 +14,3 @@
 # else:
 #     print(f"{num} is NOT a Prime Number.")
 
-
-def pyramid(rows):
-    """Pyramid Pattern"""
-    print("\nPyramid Pattern\n")
-    for i in range(1, rows + 1):
-        print(" " * (rows - i) + "* " * i)
-
-
-def inverted_pyramid(rows):
-    """Inverted Pyramid Pattern"""
-    print("\nInverted Pyramid Pattern\n")
-    for i in range(rows, 0, -1):
-        print(" " * (rows - i) + "* " * i)
-
-
-def diamond(rows):
-    """Diamond Pattern"""
-    print("\nDiamond Pattern\n")
-    # Upper part
-    for i in range(1, rows + 1):
-        print(" " * (rows - i) + "* " * i)
-    # Lower part
-    for i in range(rows - 1, 0, -1):
-        print(" " * (rows - i) + "* " * i)
-
-
-# --- Program Run ---
-rows = int(input("\nEnter number of rows for pattern: "))
-
-pyramid(rows)
-inverted_pyramid(rows)
-diamond(rows)
