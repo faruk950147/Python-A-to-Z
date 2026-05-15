99db2c24f00329cc95d319a0d32e860bf551610b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-05-15 13:17:34 +0600

    I have some changes/

--- 05 Problems Solving/main problem/classBasedWordGuess.py
+++ 05 Problems Solving/main problem/classBasedWordGuess.py
@@ -1,272 +1,274 @@
 import random
 import os
 
-# class HangmanGame:
-#     HANGMAN_PICS = [
-#         """
-#          -----
-#          |   |
-#              |
-#              |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#              |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#          |   |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#         /|   |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#         /|\\  |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#         /|\\  |
-#         /    |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#         /|\\  |
-#         / \\  |
-#              |
-#         --------
-#         """
-#     ]
+'''
+class HangmanGame:
+    HANGMAN_PICS = [
+        """
+         -----
+         |   |
+             |
+             |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+             |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+         |   |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+        /|   |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+        /|\\  |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+        /|\\  |
+        /    |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+        /|\\  |
+        / \\  |
+             |
+        --------
+        """
+    ]
 
-#     def __init__(self, word_list):
-#         self.word_list = word_list
+    def __init__(self, word_list):
+        self.word_list = word_list
 
-#     def play(self):
-#         self.word = random.choice(self.word_list).lower()
-#         self.guessed_letters = set()
-#         self.wrong_guesses = set()
-#         self.attempts = 6
+    def play(self):
+        self.word = random.choice(self.word_list).lower()
+        self.guessed_letters = set()
+        self.wrong_guesses = set()
+        self.attempts = 6
 
-#         print("Welcome to Hangman!")
-#         print(f"The word has {len(self.word)} letters.")
+        print("Welcome to Hangman!")
+        print(f"The word has {len(self.word)} letters.")
 
-#         while self.attempts > 0:
-#             display_word = "".join([letter if letter in self.guessed_letters else "_" for letter in self.word])
-#             print("\n" + self.HANGMAN_PICS[len(self.wrong_guesses)])
-#             print(f"Word: {display_word}")
-#             print(f"Wrong guesses: {', '.join(sorted(self.wrong_guesses))}")
+        while self.attempts > 0:
+            display_word = "".join([letter if letter in self.guessed_letters else "_" for letter in self.word])
+            print("\n" + self.HANGMAN_PICS[len(self.wrong_guesses)])
+            print(f"Word: {display_word}")
+            print(f"Wrong guesses: {', '.join(sorted(self.wrong_guesses))}")
 
-#             if "_" not in display_word:
-#                 print(f"\nCongratulations! You guessed the word: {self.word}")
-#                 break
+            if "_" not in display_word:
+                print(f"\nCongratulations! You guessed the word: {self.word}")
+                break
 
-#             guess = input("Guess a letter: ").lower()
+            guess = input("Guess a letter: ").lower()
 
-#             if not guess.isalpha() or len(guess) != 1:
-#                 print("Please enter a valid single letter.")
-#                 continue
+            if not guess.isalpha() or len(guess) != 1:
+                print("Please enter a valid single letter.")
+                continue
 
-#             if guess in self.guessed_letters or guess in self.wrong_guesses:
-#                 print("You already guessed that letter.")
-#                 continue
+            if guess in self.guessed_letters or guess in self.wrong_guesses:
+                print("You already guessed that letter.")
+                continue
 
-#             if guess in self.word:
-#                 self.guessed_letters.add(guess)
-#                 print("Correct!")
-#             else:
-#                 self.wrong_guesses.add(guess)
-#                 self.attempts -= 1
-#                 print(f"Wrong! Attempts left: {self.attempts}")
+            if guess in self.word:
+                self.guessed_letters.add(guess)
+                print("Correct!")
+            else:
+                self.wrong_guesses.add(guess)
+                self.attempts -= 1
+                print(f"Wrong! Attempts left: {self.attempts}")
 
-#         else:
-#             print(self.HANGMAN_PICS[-1])
-#             print(f"\nGame Over! The word was: {self.word}")
+        else:
+            print(self.HANGMAN_PICS[-1])
+            print(f"\nGame Over! The word was: {self.word}")
 
-#         # Play again option
-#         again = input("\nDo you want to play again? (y/n): ").lower()
-#         if again == "y":
-#             self.play()
-#         else:
-#             print("Thanks for playing! Goodbye.")
+        # Play again option
+        again = input("\nDo you want to play again? (y/n): ").lower()
+        if again == "y":
+            self.play()
+        else:
+            print("Thanks for playing! Goodbye.")
 
-# if __name__ == "__main__":
-#     word_list = ["apple", "banana", "cherry", "date", "elderberry"]
-#     game = HangmanGame(word_list)
-#     game.play()
+if __name__ == "__main__":
+    word_list = ["apple", "banana", "cherry", "date", "elderberry"]
+    game = HangmanGame(word_list)
+    game.play()
 
-# class AdvancedHangman:
-#     HANGMAN_PICS = [
-#         """
-#          -----
-#          |   |
-#              |
-#              |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#              |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#          |   |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#         /|   |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#         /|\\  |
-#              |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#         /|\\  |
-#         /    |
-#              |
-#         --------
-#         """,
-#         """
-#          -----
-#          |   |
-#          O   |
-#         /|\\  |
-#         / \\  |
-#              |
-#         --------
-#         """
-#     ]
+class AdvancedHangman:
+    HANGMAN_PICS = [
+        """
+         -----
+         |   |
+             |
+             |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+             |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+         |   |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+        /|   |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+        /|\\  |
+             |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+        /|\\  |
+        /    |
+             |
+        --------
+        """,
+        """
+         -----
+         |   |
+         O   |
+        /|\\  |
+        / \\  |
+             |
+        --------
+        """
+    ]
 
-#     WORDS = {
-#         "apple": "A type of fruit",
-#         "banana": "A yellow long fruit",
-#         "cherry": "Small red fruit often on top of desserts",
-#         "date": "Sweet fruit from a palm tree",
-#         "elderberry": "Dark berry used in syrups"
-#     }
+    WORDS = {
+        "apple": "A type of fruit",
+        "banana": "A yellow long fruit",
+        "cherry": "Small red fruit often on top of desserts",
+        "date": "Sweet fruit from a palm tree",
+        "elderberry": "Dark berry used in syrups"
+    }
 
-#     def __init__(self):
-#         self.score = 0
+    def __init__(self):
+        self.score = 0
 
-#     def choose_word(self, difficulty):
-#         words = list(self.WORDS.keys())
-#         if difficulty == "easy":
-#             words = [w for w in words if len(w) <= 5]
-#         elif difficulty == "medium":
-#             words = [w for w in words if 5 < len(w) <= 7]
-#         else:  # hard
-#             words = [w for w in words if len(w) > 7]
-#         return random.choice(words)
+    def choose_word(self, difficulty):
+        words = list(self.WORDS.keys())
+        if difficulty == "easy":
+            words = [w for w in words if len(w) <= 5]
+        elif difficulty == "medium":
+            words = [w for w in words if 5 < len(w) <= 7]
+        else:  # hard
+            words = [w for w in words if len(w) > 7]
+        return random.choice(words)
 
-#     def play(self):
-#         print("Welcome to Advanced Hangman!")
-#         difficulty = input("Choose difficulty (easy/medium/hard): ").lower()
-#         self.word = self.choose_word(difficulty)
-#         self.hint = self.WORDS[self.word]
-#         self.guessed_letters = set()
-#         self.wrong_guesses = set()
-#         self.attempts = 6
+    def play(self):
+        print("Welcome to Advanced Hangman!")
+        difficulty = input("Choose difficulty (easy/medium/hard): ").lower()
+        self.word = self.choose_word(difficulty)
+        self.hint = self.WORDS[self.word]
+        self.guessed_letters = set()
+        self.wrong_guesses = set()
+        self.attempts = 6
 
-#         print(f"The word has {len(self.word)} letters. Hint: {self.hint}")
+        print(f"The word has {len(self.word)} letters. Hint: {self.hint}")
 
-#         while self.attempts > 0:
-#             display_word = "".join([letter if letter in self.guessed_letters else "_" for letter in self.word])
-#             print("\n" + self.HANGMAN_PICS[len(self.wrong_guesses)])
-#             print(f"Word: {display_word}")
-#             print(f"Wrong guesses: {', '.join(sorted(self.wrong_guesses))}")
-#             print(f"Score: {self.score}")
+        while self.attempts > 0:
+            display_word = "".join([letter if letter in self.guessed_letters else "_" for letter in self.word])
+            print("\n" + self.HANGMAN_PICS[len(self.wrong_guesses)])
+            print(f"Word: {display_word}")
+            print(f"Wrong guesses: {', '.join(sorted(self.wrong_guesses))}")
+            print(f"Score: {self.score}")
 
-#             if "_" not in display_word:
-#                 print(f"\nYou guessed the word: {self.word}")
-#                 self.score += 10 + len(self.word) - len(self.wrong_guesses)
-#                 print(f"Your score: {self.score}")
-#                 break
+            if "_" not in display_word:
+                print(f"\nYou guessed the word: {self.word}")
+                self.score += 10 + len(self.word) - len(self.wrong_guesses)
+                print(f"Your score: {self.score}")
+                break
 
-#             guess = input("Guess a letter: ").lower()
-#             if not guess.isalpha() or len(guess) != 1:
-#                 print("Enter a valid single letter.")
-#                 continue
-#             if guess in self.guessed_letters or guess in self.wrong_guesses:
-#                 print("You already guessed that letter.")
-#                 continue
+            guess = input("Guess a letter: ").lower()
+            if not guess.isalpha() or len(guess) != 1:
+                print("Enter a valid single letter.")
+                continue
+            if guess in self.guessed_letters or guess in self.wrong_guesses:
+                print("You already guessed that letter.")
+                continue
 
-#             if guess in self.word:
-#                 self.guessed_letters.add(guess)
-#                 print("Correct!")
-#             else:
-#                 self.wrong_guesses.add(guess)
-#                 self.attempts -= 1
-#                 print(f"Wrong! Attempts left: {self.attempts}")
+            if guess in self.word:
+                self.guessed_letters.add(guess)
+                print("Correct!")
+            else:
+                self.wrong_guesses.add(guess)
+                self.attempts -= 1
+                print(f"Wrong! Attempts left: {self.attempts}")
 
-#         else:
-#             print(self.HANGMAN_PICS[-1])
-#             print(f"\nGame Over! The word was: {self.word}")
-#             print(f"Your score: {self.score}")
+        else:
+            print(self.HANGMAN_PICS[-1])
+            print(f"\nGame Over! The word was: {self.word}")
+            print(f"Your score: {self.score}")
 
-#         again = input("\nPlay again? (y/n): ").lower()
-#         if again == "y":
-#             self.play()
-#         else:
-#             print("Thanks for playing! Goodbye.")
+        again = input("\nPlay again? (y/n): ").lower()
+        if again == "y":
+            self.play()
+        else:
+            print("Thanks for playing! Goodbye.")
 
-# if __name__ == "__main__":
-#     game = AdvancedHangman()
-#     game.play()
+if __name__ == "__main__":
+    game = AdvancedHangman()
+    game.play()
 
+'''
 
 
 # =============== class WordGuessGame ===================
