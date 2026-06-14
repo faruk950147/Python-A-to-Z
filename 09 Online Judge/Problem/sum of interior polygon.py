46c7e5f6d78b007aedf62354847f9cc64fc1e218
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-06-14 06:59:39 +0600

    I have some changes

---
+++ 09 Online Judge/Problem/sum of interior polygon.py
@@ -0,0 +1,9 @@
+'''
+polygon is a closed shape with straight lines 
+sum of interior angles = (n - 2) * 180
+'''
+n = int(input())
+
+sum_of_angles = (n - 2) * 180
+
+print(sum_of_angles)
---
+++ sum of n numbers.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+sum = 0
+count = 0
+for i in range(1, n + 1):
+    sum += i
+    count += 1
+print("Sum:", sum)
+print("Count:", count)
