c1c355773f13c0c7b20c2e01e6f1c1f227657e02
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-07-05 19:49:22 +0600

    I have some changes

--- 05 Problems Solving/main problem/distance two points.py
+++ 05 Problems Solving/main problem/distance two points.py
@@ -1,7 +1,25 @@
-'''
-how to calculate the distance between two points
-'''
-def distance(x1, y1, x2, y2):
-    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+class Point:
+    '''
+    how to calculate the distance between two points
 
-print(distance(0, 0, 3, 4))
+    formula: d = √[(x2 - x1)² + (y2 - y1)²]
+    x1 = 0
+    y1 = 0
+    x2 = 3
+    y2 = 4
+
+    (3 - 0) ** 2 + (4 - 0) ** 2
+    (3) ** 2 + (4) ** 2
+    9 + 16 = 25
+    √25 = 5
+
+
+    '''
+    def __init__(self):
+        pass
+    
+    def distance(self, x1, x2, y1, y2):
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+p1 = Point()
+print(p1.distance(0, 3, 0, 4))
