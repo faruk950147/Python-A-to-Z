1cca2221d612baffbbd040e35a415a489bd590a7
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-07-10 10:24:39 +0600

    I have some changes

---
+++ 03. Intermediate Level/01 Built-in/sets/main.py
@@ -0,0 +1,64 @@
+# ============================= Topic: Sets - Subset and Superset =============================
+'''
+# A is a main set
+A = {1, 2, 3, 4, 5, 6}
+
+# B is a subset of A
+B = {1, 2}
+
+print(f"B is a subset of A: {B < A}")
+print(f"A is a superset of B: {A > B}")
+# methods
+print(f"B is a subset of A: {B.issubset(A)}")
+print(f"A is a superset of B: {A.issuperset(B)}")
+
+# ============================= Topic: Sets - Proper Subset and Proper Superset =============================
+
+# A is a main set formula 2^n (n is the number of elements in the set)
+A = {1, 2, 3, 4, 5, 6}
+
+# A has 6 elements, so it has 2^6 = 64 subsets
+# A proper subset is a subset that is not equal to the original set
+
+# B is a proper subset of A
+B = {1, 2}
+
+print(f"B is a proper subset of A: {B < A}")
+print(f"A is a proper superset of B: {A > B}")
+# methods
+print(f"B is a proper subset of A: {B.issubset(A)}")
+print(f"A is a proper superset of B: {A.issuperset(B)}")
+
+# ============================= Topic: Sets - Universal Sets =============================
+
+universal_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+
+# ============================= Topic: Sets - Complement Sets =============================
+
+complement_set = universal_set - {1, 2, 3}
+print(f"Complement of {1, 2, 3} in universal set: {complement_set}")
+
+
+'''
+
+# ============================= Topic: Sets - Operations =============================
+
+# Union is the combination of two sets
+print(f"Union of {1, 2, 3} and {3, 4, 5}: {1, 2, 3} | {3, 4, 5}")
+
+# Intersection is the common elements between two sets
+print(f"Intersection of {1, 2, 3} and {3, 4, 5}: {1, 2, 3} & {3, 4, 5}")
+
+# Difference is the elements that are in the first set but not in the second set
+print(f"Difference of {1, 2, 3} and {3, 4, 5}: {1, 2, 3} - {3, 4, 5}")
+
+# Symmetric Difference is the elements that are in either set but not in both
+print(f"Symmetric difference of {1, 2, 3} and {3, 4, 5}: {1, 2, 3} ^ {3, 4, 5}")
+
+# Disjoint means that the sets have no common elements
+print(f"Are {1, 2, 3} and {4, 5, 6} disjoint? {1, 2, 3}.isdisjoint({4, 5, 6})")
+
+
+
+
