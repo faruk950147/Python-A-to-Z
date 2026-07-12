3205356683a00548b3e5dc911736a5344207a55a
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-07-12 10:36:50 +0600

    I have some changes

--- 03. Intermediate Level/01 Built-in/sets/main.py
+++ 03. Intermediate Level/01 Built-in/sets/main.py
@@ -43,26 +43,28 @@ print(f"Complement of {1, 2, 3} in universal set: {complement_set}")
 '''
 
 # ============================= Topic: Sets - Operations =============================
+A = {1, 2, 3}
+B = {3, 4, 5}   
 
 # Union is the combination of two sets
-print(f"Union of {1, 2, 3} and {3, 4, 5}: {1, 2, 3} | {3, 4, 5}")
+print(f"Union of {A} and {B}: {A | B}")
 # output: {1, 2, 3, 4, 5}
 
 # Intersection is the common elements between two sets
-print(f"Intersection of {1, 2, 3} and {3, 4, 5}: {1, 2, 3} & {3, 4, 5}")
+print(f"Intersection of {A} and {B}: {A & B}")
 # output: {3}
 
 # Difference is the elements that are in the first set but not in the second set
-print(f"Difference of {1, 2, 3} and {3, 4, 5}: {1, 2, 3} - {3, 4, 5}")
+print(f"Difference of {A} and {B}: {A - B}")
 # output: {1, 2}
-    
+
 # Symmetric Difference is the elements that are in either set but not in both
-print(f"Symmetric difference of {1, 2, 3} and {3, 4, 5}: {1, 2, 3} ^ {3, 4, 5}")
+print(f"Symmetric difference of {A} and {B}: {A ^ B}")
 # output: {1, 2, 4, 5}
 
 # Disjoint means that the sets have no common elements
-print(f"Are {1, 2, 3} and {4, 5, 6} disjoint? {1, 2, 3}.isdisjoint({4, 5, 6})")
-# output: True
+print(f"Are {A} and {B} disjoint? {A.isdisjoint(B)}")
+# output: False
 
 
 
