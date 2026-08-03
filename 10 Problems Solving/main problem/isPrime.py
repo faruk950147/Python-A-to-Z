ff8c0e7ba302346460ff4c1026d14062a4c9fbc1
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-03 12:05:06 +0600

    I have added selection sort

--- 10 Problems Solving/main problem/isPrime.py
+++ 10 Problems Solving/main problem/isPrime.py
@@ -1,15 +1,16 @@
-def Is_Prime(n):
-    if n <= 1 or n == 0:
+def is_prime(n):
+    """Check if a number is prime"""
+    if n < 2:
         return False
-    else:
-        for i in range(2, n):
-            if n % i == 0:
-                return False
-        return True
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+# --- Program Run ---
+num = int(input("Enter a number to check if it's prime: "))
 
-n = int(input("Enter a number: "))
-for i in range(2, n):
-    if Is_Prime(i):
-        print(f'{i} is a prime number')
-    else:
-        print(f'{i} is not a prime number')
+if is_prime(num):
+    print(f"{num} is a Prime Number.")
+else:
+    print(f"{num} is NOT a Prime Number.")
+
