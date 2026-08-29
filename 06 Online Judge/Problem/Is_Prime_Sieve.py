ddb2fb81056b7f5c5efc2e523609392f319ca994
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-08-29 18:59:03 +0600

    It's my live portfolio

--- 06 Online Judge/Problem/Is_Prime_Sieve.py
+++ 06 Online Judge/Problem/Is_Prime_Sieve.py
@@ -327,3 +327,31 @@ def sieve(n):
 
 
 print(sieve(30))
+
+# Normal
+class IsPrime:
+
+    def is_prime(self, n):
+        """Check if a number is prime"""
+
+        if n < 2:
+            return False
+
+        for i in range(2, int(n ** 0.5) + 1):
+
+            if n % i == 0:
+                return False
+
+        return True
+
+
+# --- Program Run ---
+
+prime_checker = IsPrime()
+
+num = int(input("Enter a number to check if it's prime: "))
+
+if prime_checker.is_prime(num):
+    print(f"{num} is a Prime Number.")
+else:
+    print(f"{num} is NOT a Prime Number.")
