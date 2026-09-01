2b01f83d66808ed230e26d36cb26849296cb48e2
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-01 08:03:58 +0600

    It's my live portfolio

--- 06 Online Judge/HackerRank/4 Python division.py
+++
@@ -1,8 +0,0 @@
-def perform_division(a, b):
-    print(a // b)  # Integer division 
-    print(a / b)   # Float division 
-
-if __name__ == '__main__':
-    a = int(input())  # first number input
-    b = int(input())  # second number input
-    perform_division(a, b)  
